d7b34cb6c41c6f87cc66ecbe0d774cdc01266357
Fede <i@2fd.me>
2020-03-02 16:07:47 -0300

    chore: setup project

---
+++ .storybook/main.js
@@ -0,0 +1,43 @@
+const { resolve } = require('path')
+const { readFileSync } = require('fs')
+const prettierConfig = JSON.parse(
+  readFileSync(resolve(__dirname, '../.prettierrc'), 'utf8')
+)
+
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-actions/register',
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [resolve(__dirname, '../src')], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig,
+        },
+      },
+    },
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+}
---
+++ .storybook/preview.js
@@ -0,0 +1,4 @@
+require("semantic-ui-css/semantic.min.css")
+require("balloon-css/balloon.min.css")
+require("decentraland-ui/src/themes/base-theme.css")
+require("decentraland-ui/src/themes/alternative/light-theme.css")
