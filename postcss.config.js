3454624788a355cc5d6708404baedcffa703cafe
Fede <i@2fd.me>
2020-03-13 13:42:50 -0300

    chore: compress css on build

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("autoprefixer"), require("postcss-svg")],
+  plugins: [
+    require('autoprefixer'),
+    require('cssnano'),
+    require('postcss-svg'),
+  ],
 }
