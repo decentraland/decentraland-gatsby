0ea0cdb34e8370b5e29fc6d38b45a08e475398c2
Fede <i@2fd.me>
2020-03-17 14:48:18 -0300

    fix: read gatsby environment variable

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,3 @@
 export default function env(name: string, defaultValue: string): string {
-  return process.env[name] || process.env['REACT_APP_' + name] || process.env['STORYBOOK_' + name] || defaultValue
+  return process.env[name] || process.env['GATSBY_' + name] || process.env['REACT_APP_' + name] || process.env['STORYBOOK_' + name] || defaultValue
 }
