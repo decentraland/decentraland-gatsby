40682035e0192e510d73f4a53d2fb03c1163fedb
Fede <i@2fd.me>
2020-03-18 15:18:01 -0300

    feat: add useLocation

---
+++ src/hooks/useLocation.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import SingletonListener from '../utils/SingletonListener'
+
+export default function useLocation() {
+  const [location, setLocation] = useState<URL | null>(null)
+
+  useEffect(() => {
+    const listener = SingletonListener.from(window)
+    const changeLocation = () => setLocation(new URL(window.location.toString()))
+
+    listener.addEventListener('hashchange' as any, changeLocation)
+    listener.addEventListener('popstate' as any, changeLocation)
+    changeLocation()
+
+    return () => {
+      listener.removeEventListener('hashchange' as any, changeLocation)
+      listener.removeEventListener('popstate' as any, changeLocation)
+    }
+  }, [])
+
+  return [location]
+}
