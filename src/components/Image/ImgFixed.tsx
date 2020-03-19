f917f6d8c3decc480a4643f07b075605372437f9
Fede <i@2fd.me>
2020-03-19 07:41:00 -0300

    feat: add components/Image/ImgFixed

---
+++ src/components/Image/ImgFixed.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import classname from '../../utils/classname'
+import { StyleNamespace } from '../../variables'
+
+import './ImgFixed.css'
+
+export type ImgFixedProps = Omit<React.HTMLProps<HTMLDivElement>, 'size'> & {
+  dimension?: 'square' | 'wide'
+  position?: string
+  size?: 'cover' | 'contain'
+  background?: string
+}
+
+export default function ImgFixed({ src, dimension, size, background, position, ...props }: ImgFixedProps) {
+  return <div {...props}
+    className={classname([StyleNamespace, 'ImgFixed', props.className])}
+    style={{
+      ...props.style,
+      backgroundColor: background || 'transparent',
+      backgroundSize: size || 'cover',
+      backgroundPosition: position || 'center center',
+      backgroundImage: src && `url("${src}")`
+    }}
+  >
+    {(!dimension || dimension === 'square') && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4AWNwL/BhAAADCQEEiiqnjQAAAABJRU5ErkJggg==" width="1" height="1" />}
+    {dimension === 'wide' && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAABCAYAAAD0In+KAAAADUlEQVR4AWNwL/ABYwAKHQIHW//QwwAAAABJRU5ErkJggg==" width="2" height="1" />}
+  </div>
+}
---
+++ src/components/Image/index.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { storiesOf } from "@storybook/react";
+import centered from '@storybook/addon-centered/react'
+import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid'
+import Divider from '../Text/Divider';
+import ImgFixed from './ImgFixed'
+import Title from '../Text/Title';
+import Code from '../Text/Code';
+import { Container } from 'decentraland-ui/dist/components/Container/Container';
+import Paragraph from '../Text/Paragraph';
+import SubTitle from '../Text/SubTitle';
+
+storiesOf('Images', module)
+  .addDecorator(centered)
+  .add('ImgAvatar', () => {
+    return <Container>
+      <Divider />
+      <Title><Code inline>ImgFixed</Code> component:  </Title>
+      <Paragraph secondary>Allow you insert an image with a constant relation between width and height</Paragraph>
+      <Divider size="small" />
+      <Grid>
+        <Grid.Row>
+          <Grid.Column mobile="6">
+            <SubTitle>Wide</SubTitle>
+            <ImgFixed dimension="wide" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+          </Grid.Column>
+          <Grid.Column mobile="6">
+            <SubTitle>Square</SubTitle>
+            <ImgFixed dimension="square" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+          </Grid.Column>
+        </Grid.Row >
+      </Grid >
+      <Divider />
+    </Container >
+  })
