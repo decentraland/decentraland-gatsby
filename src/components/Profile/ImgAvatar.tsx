295f930d0901e2b9772b276b21ef6a2fe1d4be22
Fede <i@2fd.me>
2020-03-20 08:04:57 -0300

    feat: add address prop to ImgAvatar

--- src/components/Profile/ImgAvatar.tsx
+++ src/components/Profile/ImgAvatar.tsx
@@ -9,11 +9,12 @@ const DEFAULT_AVATAR = 'https://peer.decentraland.org/content/contents/QmVfTSvEJ
 
 export type ImgAvatarProps = Omit<React.HTMLProps<HTMLImageElement>, 'src' | 'height' | 'width' | 'size'> & {
   profile?: Profile,
+  address?: string,
   size?: 'mini' | 'tiny' | 'small' | 'medium' | 'large' | 'big' | 'huge' | 'massive' | 'full'
 }
 
-export default function ImgAvatar({ profile, size, ...props }: ImgAvatarProps) {
+export default function ImgAvatar({ profile, address, size, ...props }: ImgAvatarProps) {
   const src = profile?.avatar?.avatar?.snapshots?.face || DEFAULT_AVATAR
-  const init = (profile?.address || '0x0')[2]
+  const init = (profile?.address || address || '0x0')[2]
   return <img {...props as any} src={src} width="100" height="100" className={classname([StyleNamespace, 'ImgAvatar', `ImgAvatar--${size}`, `ImgAvatar--${init}`, props.className])} />
 }
