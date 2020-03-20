8dfa1dea0245cb254cebb612c8d2c7d82f79c568
Fede <i@2fd.me>
2020-03-20 09:30:41 -0300

    feat: add hooks/useEntityStoreSelector

---
+++ src/hooks/useEntityStoreSelector.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect, useMemo } from "react"
+import EntityStore, { EntityStoreState } from "../utils/EntityStore"
+
+export type Selector<E extends object, R> = (store: EntityStoreState<E>) => R
+
+const defaultSelector = (state: EntityStoreState<any>) => state as any
+
+export default function useEntityStoreSelector<E extends object, R>(
+  store: EntityStore<E>,
+  selector: (store: EntityStoreState<E>) => R = defaultSelector
+) {
+  const [state, setState] = useState(store.getState())
+  const result = useMemo(() => selector(state), [state, selector])
+
+  useEffect(() => {
+    function handleChange(newState: EntityStoreState<E>) {
+      if (state !== newState) {
+        setState(newState)
+      }
+    }
+
+    store.addEventListener('change', handleChange)
+    return function removeStoreListener() {
+      store.removeEventListener('change', handleChange)
+    }
+  }, [])
+
+  return result
+}
