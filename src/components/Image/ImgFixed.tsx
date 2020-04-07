cbc95fc832362c5d201263eee0f7236bf325ddac
Fede <i@2fd.me>
2020-04-07 08:05:47 -0300

    feat: add dimension="vertical" option to ImgFixed

--- src/components/Image/ImgFixed.tsx
+++ src/components/Image/ImgFixed.tsx
@@ -5,7 +5,7 @@ import { StyleNamespace } from '../../variables'
 import './ImgFixed.css'
 
 export type ImgFixedProps = Omit<React.HTMLProps<HTMLDivElement>, 'size'> & {
-  dimension?: 'square' | 'wide'
+  dimension?: 'square' | 'wide' | 'vertical'
   position?: string
   size?: 'cover' | 'contain'
   background?: string
@@ -24,5 +24,6 @@ export default function ImgFixed({ src, dimension, size, background, position, .
   >
     {(!dimension || dimension === 'square') && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4AWNwL/BhAAADCQEEiiqnjQAAAABJRU5ErkJggg==" width="1" height="1" />}
     {dimension === 'wide' && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAABCAYAAAD0In+KAAAADUlEQVR4AWNwL/ABYwAKHQIHW//QwwAAAABJRU5ErkJggg==" width="2" height="1" />}
+    {dimension === 'vertical' && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAADklEQVR4AWNwL/BhABMACyECB9kZjBwAAAAASUVORK5CYII=" width="1" height="2" />}
   </div>
 }
--- src/components/Image/index.stories.tsx
+++ src/components/Image/index.stories.tsx
@@ -20,13 +20,17 @@ storiesOf('Images', module)
       <Divider size="small" />
       <Grid>
         <Grid.Row>
-          <Grid.Column mobile="6">
+          <Grid.Column mobile="5">
             <SubTitle>Wide</SubTitle>
             <ImgFixed dimension="wide" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
           </Grid.Column>
-          <Grid.Column mobile="6">
+          <Grid.Column mobile="5">
             <SubTitle>Square</SubTitle>
             <ImgFixed dimension="square" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+          </Grid.Column>
+          <Grid.Column mobile="5">
+            <SubTitle>Vertical</SubTitle>
+            <ImgFixed dimension="vertical" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
           </Grid.Column>
         </Grid.Row >
       </Grid >
