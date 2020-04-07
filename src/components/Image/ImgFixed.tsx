f11a9533f24039de06c9cb3969cf7f74675254bb
Fede <i@2fd.me>
2020-04-07 08:20:35 -0300

    fix: ImgFixed vertical

--- src/components/Image/ImgFixed.tsx
+++ src/components/Image/ImgFixed.tsx
@@ -24,6 +24,6 @@ export default function ImgFixed({ src, dimension, size, background, position, .
   >
     {(!dimension || dimension === 'square') && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4AWNwL/BhAAADCQEEiiqnjQAAAABJRU5ErkJggg==" width="1" height="1" />}
     {dimension === 'wide' && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAABCAYAAAD0In+KAAAADUlEQVR4AWNwL/ABYwAKHQIHW//QwwAAAABJRU5ErkJggg==" width="2" height="1" />}
-    {dimension === 'vertical' && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZgbYnAAAADklEQVR4AWNwL/BhABMACyECB9kZjBwAAAAASUVORK5CYII=" width="1" height="2" />}
+    {dimension === 'vertical' && <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAIAAAADCAYAAAC56t6BAAAAD0lEQVR4AWNwL/ABY0wGAFT5BhPIHHvgAAAAAElFTkSuQmCC" width="2" height="3" />}
   </div>
 }
