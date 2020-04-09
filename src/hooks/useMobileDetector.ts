1d59df49549f1679c5be712f02461be778c25d6b
Fede <i@2fd.me>
2020-04-09 14:54:36 -0300

    refactor: improve mobile detection

--- src/hooks/useMobileDetector.ts
+++ src/hooks/useMobileDetector.ts
@@ -1,19 +1,18 @@
 import { useState, useEffect } from 'react'
 import isMobile from '../utils/isMobile'
 
-let IS_MOBILE = false
+let IS_MOBILE: boolean | null = null
 
-export default function useMobileDetector(initialValue: boolean = IS_MOBILE) {
+export default function useMobileDetector(initialValue: boolean = IS_MOBILE ?? false) {
   const [mobile, setMobile] = useState<boolean>(initialValue)
 
   useEffect(() => {
-    const value = isMobile()
-    if (value !== IS_MOBILE) {
-      IS_MOBILE = value
-    }
+    if (IS_MOBILE === null) {
+      IS_MOBILE = isMobile()
 
-    if (value !== mobile) {
-      setMobile(IS_MOBILE)
+      if (IS_MOBILE !== mobile) {
+        setMobile(IS_MOBILE)
+      }
     }
   }, [])
 
