0e3bae6d371ecb3da2b4f94ac61e72cdf370b06e
Fede <i@2fd.me>
2020-05-26 10:58:21 -0300

    feat: add requiredEnv function

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,3 +1,13 @@
 export default function env(name: string, defaultValue: string): string {
   return process.env[name] || process.env['GATSBY_' + name] || process.env['REACT_APP_' + name] || process.env['STORYBOOK_' + name] || defaultValue
 }
+
+export function requiredEnv(name: string): string {
+  const value = env(name, '')
+
+  if (!value) {
+    throw new Error(`Missing "${name}" environment variable. Check your .env.example file`)
+  }
+
+  return value
+}
