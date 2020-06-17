be1a97f52242a2dd3cac88dbc46ae8304f6c2029
Fede <i@2fd.me>
2020-06-17 18:08:44 -0300

    fix: setup command

--- bin/setup.js
+++ bin/setup.js
@@ -2,89 +2,124 @@
 const { resolve } = require('path')
 const { sync } = require('glob')
 const { mkdirSync, writeFileSync, readFileSync } = require('fs')
-const { spawnSync } = require('child_process')
-const { grey, green } = require('colors/safe')
+const { spawn } = require('child_process')
+const { grey, green, red } = require('colors/safe')
 
-spawnSync('npm', [
-  'install',
-  '-s',
-  'dotenv',
-  'express',
-  'web3x',
-  'dcl-crypto',
-  'decentraland-crypto',
-  'node-pg-migrate',
-  'isomorphic-fetch',
-  'nodemon',
-  'pg',
-  'ajv',
-  'body-parser',
-  'gatsby',
-  'gatsby-image',
-  'gatsby-plugin-intl',
-  'gatsby-plugin-manifest',
-  'gatsby-plugin-offline',
-  'gatsby-plugin-react-helmet',
-  'gatsby-plugin-sass',
-  'gatsby-plugin-sharp',
-  'gatsby-plugin-typescript',
-  'gatsby-source-filesystem',
-  'gatsby-transformer-sharp',
-  'validator',
-])
-
-spawnSync('npm', [
-  'install ',
-  '-D',
-  'prettier',
-  'concurrently',
-  'ts-node',
-  '@types/validator',
-  '@types/node',
-  '@types/isomorphic-fetch',
-  '@types/express',
-])
-
-const pkg = require(resolve(process.cwd(), 'package.json'))
-pkg.scripts = Object.assign(pkg.scripts || {}, {
-  build: 'gatsby build && tsc -p .',
-  develop: 'gatsby develop -H 0.0.0.0',
-  format: 'prettier --write "**/*.{js,jsx,json,md}"',
-  start:
-    "concurrently -c blue,green -n SERVER,FRONT 'npm run serve' 'npm run develop'",
-  serve:
-    "DOTENV_CONFIG_PATH=.env.development nodemon --watch src/entities --watch src/server.ts -e ts,json --exec 'ts-node -r dotenv/config.js' src/server",
-  clean: 'gatsby clean',
-  migrate:
-    'DOTENV_CONFIG_PATH=.env.development ts-node -r dotenv/config.js ./node_modules/node-pg-migrate/bin/node-pg-migrate -j ts -d CONNECTION_STRING',
-  production: 'NODE_ENV=production node lib/server.js',
-  test: 'echo "Write tests! -> https://gatsby.dev/unit-testing" && exit 1',
-})
-writeFileSync(
-  resolve(process.cwd(), 'package.json'),
-  JSON.stringify(pkg, null, 2)
-)
-
-const templates = __dirname + '/templates'
-const files = sync('**/*', {
-  cwd: templates,
-  root: templates,
-  absolute: false,
-  dot: true,
-  mark: true,
-})
-
-files.forEach(file => {
-  const isDir = file.endsWith('/')
-  const target = isDir ? file.slice(0, -1) : file
-  console.log(grey('creating'), green(target))
-
-  if (isDir) {
-    mkdirSync(resolve(process.cwd(), target))
-  } else {
-    writeFileSync(
-      resolve(process.cwd(), target),
-      readFileSync(resolve(templates, target))
+function spawnPromise(command, args, options) {
+  return new Promise((resolve, reject) => {
+    const run = spawn(command, args, options)
+    run.on('exit', code =>
+      code
+        ? reject(new Error(`Command ${command} exit with code ${code}`))
+        : resolve(0)
     )
-  }
-})
+  })
+}
+
+Promise.resolve()
+  .then(() =>
+    spawnPromise(
+      'npm',
+      [
+        'install',
+        '-s',
+        'dotenv',
+        'express',
+        'web3x',
+        'dcl-crypto',
+        'decentraland-crypto',
+        'node-pg-migrate',
+        'isomorphic-fetch',
+        'nodemon',
+        'pg',
+        'ajv',
+        'body-parser',
+        'gatsby',
+        'gatsby-image',
+        'gatsby-plugin-intl',
+        'gatsby-plugin-manifest',
+        'gatsby-plugin-offline',
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sass',
+        'gatsby-plugin-sharp',
+        'gatsby-plugin-typescript',
+        'gatsby-source-filesystem',
+        'gatsby-transformer-sharp',
+        'validator',
+      ],
+      {}
+    )
+  )
+  .then(() =>
+    spawnSync('npm', [
+      'install ',
+      '-D',
+      'prettier',
+      'concurrently',
+      'ts-node',
+      '@types/validator',
+      '@types/node',
+      '@types/isomorphic-fetch',
+      '@types/express',
+    ])
+  )
+  .then(() => {
+    const pkg = require(resolve(process.cwd(), 'package.json'))
+    pkg.scripts = Object.assign(pkg.scripts || {}, {
+      build: 'gatsby build && tsc -p .',
+      develop: 'gatsby develop -H 0.0.0.0',
+      format: 'prettier --write "**/*.{js,jsx,json,md}"',
+      start:
+        "concurrently -c blue,green -n SERVER,FRONT 'npm run serve' 'npm run develop'",
+      serve:
+        "DOTENV_CONFIG_PATH=.env.development nodemon --watch src/entities --watch src/server.ts -e ts,json --exec 'ts-node -r dotenv/config.js' src/server",
+      clean: 'gatsby clean',
+      migrate:
+        'DOTENV_CONFIG_PATH=.env.development ts-node -r dotenv/config.js ./node_modules/node-pg-migrate/bin/node-pg-migrate -j ts -d CONNECTION_STRING',
+      production: 'NODE_ENV=production node lib/server.js',
+      test: 'echo "Write tests! -> https://gatsby.dev/unit-testing" && exit 1',
+    })
+    writeFileSync(
+      resolve(process.cwd(), 'package.json'),
+      JSON.stringify(pkg, null, 2)
+    )
+  })
+  .then(() => {
+    const templates = __dirname + '/templates'
+    const files = sync('**/*', {
+      cwd: templates,
+      root: templates,
+      absolute: false,
+      mark: true,
+    })
+
+    files.forEach(file => {
+      const isDir = file.endsWith('/')
+      const target = isDir ? file.slice(0, -1) : file
+      console.log(grey('creating'), green(target))
+
+      if (isDir) {
+        mkdirSync(resolve(process.cwd(), target))
+      } else {
+        writeFileSync(
+          resolve(process.cwd(), target),
+          readFileSync(resolve(templates, target))
+        )
+      }
+    })
+  })
+  .then(() => console.log('\n', green(`Done! Have a nice day!`, '\n')))
+  .catch(err => {
+    console.log()
+    console.error(red(`    ${err.message}`))
+    console.error(
+      grey(
+        err.stack
+          .split('\n')
+          .map(line => '    ' + line)
+          .join('\n')
+      )
+    )
+    console.log()
+    process.exist(1)
+  })
---
+++ bin/setup.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+node -r 'decentraland-gatsby/bin/setup.js'
