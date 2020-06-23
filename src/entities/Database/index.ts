3ac723838e174ed710b2365b6182f9b9914e9fed
Fede <i@2fd.me>
2020-06-23 12:55:04 -0300

    feat: add schema param

--- src/entities/Database/index.ts
+++ src/entities/Database/index.ts
@@ -1,5 +1,6 @@
 import { db } from 'decentraland-server'
 import env from '../../utils/env'
+import { parse } from 'url'
 
 const pg = db.clients.postgres
 
@@ -12,7 +13,22 @@ pg.setTypeParser(1114, (date: string) => {
 
 database.connect = async () => {
   const CONNECTION_STRING = env('CONNECTION_STRING', '')
-  return pg.connect(CONNECTION_STRING)
+  const client = await pg.connect(CONNECTION_STRING)
+
+  try {
+    const url = parse(CONNECTION_STRING)
+    if (url.query) {
+      const params = new URLSearchParams(url.query || '')
+      const schema = params.get('schema')
+      if (schema) {
+        await client.query(`SET search_path TO ${schema}`)
+      }
+    }
+  } catch (err) {
+    console.error(err)
+  }
+
+  return client
 }
 
 export default database;
