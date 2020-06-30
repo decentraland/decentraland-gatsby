d18bc7209827cf1a0d82bcb76e8f56397438f6d2
Fede <i@2fd.me>
2020-06-30 01:06:54 -0300

    feat: add api types

---
+++ src/utils/api/types.ts
@@ -0,0 +1,3 @@
+export type ApiResponse<D> = { ok: boolean, data: D }
+export type ApiError<D = undefined> = { ok: boolean, error: string, data: D }
+export type ApiValidationError = { ok: boolean, error: string, data: { errors: string[] } }
