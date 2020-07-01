1621b07d20ec8fa3068c0cedbd2131e1011d110f
Fede <i@2fd.me>
2020-07-01 09:29:12 -0300

    feat: add entities/Blocks

---
+++ src/entities/Blockchain/eth.ts
@@ -0,0 +1,42 @@
+import { parse } from 'url'
+import { Eth } from 'web3x/eth';
+import { HttpProvider, WebsocketProvider } from 'web3x/providers';
+import { createItemPool } from '../Pool/utils';
+import env from '../../utils/env';
+
+const ETHEREUM_ENDPOINT = env('ETHEREUM_ENDPOINT', '')
+
+export function getCurrentProviders() {
+  return ETHEREUM_ENDPOINT
+    .split(',')
+    .map((endpoint) => {
+      const url = parse(endpoint)
+      switch (url.protocol) {
+        case 'wss:':
+          return new WebsocketProvider(endpoint);
+
+        case 'https:':
+          return new HttpProvider(endpoint)
+
+        case null:
+          return null
+
+        default:
+          throw new Error(`Invalid ethereum endpoint protocol: "${url.protocol}"`);
+      }
+    })
+    .filter(Boolean) as (WebsocketProvider | HttpProvider)[]
+}
+
+export function getEths() {
+  return getCurrentProviders().map(provider => new Eth(provider))
+}
+
+export function getEthPool() {
+  const eth = getEths()
+  if (eth.length === 0) {
+    console.log(`creating empty pool`)
+  }
+
+  return createItemPool(eth)
+}
