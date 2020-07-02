edde05bea796e6353fad0b080ae435c1a2b98cd9
Fede <i@2fd.me>
2020-07-02 16:34:52 -0300

    fix: utils/arr/unique map props

--- src/utils/array/unique.test.ts
+++ src/utils/array/unique.test.ts
@@ -1,5 +1,6 @@
 import unique from "./unique"
 
 test('utils/array/unique', () => {
-  expect(Array.from(unique([1, 2, 2, 3, 3, 3]))).toEqual([1, 2, 3])
+  expect(Array.from(unique([1, 2, 2]))).toEqual([1, 2])
+  expect(Array.from(unique([{ value: 1 }, { value: 2 }, { value: 2 }], (o) => o.value))).toEqual([1, 2])
 })
--- src/utils/array/unique.ts
+++ src/utils/array/unique.ts
@@ -2,11 +2,12 @@ export default function* unique<Input, Output = Input>(
   arr: Input[],
   map: (value: Input) => Output = (v) => v as any
 ) {
-  const prev = new Set<Input>()
+  const prev = new Set<Output>()
   for (const values of arr) {
-    if (!prev.has(values)) {
-      prev.add(values)
-      yield map(values)
+    const output = map(values)
+    if (!prev.has(output)) {
+      prev.add(output)
+      yield output
     }
   }
 }
