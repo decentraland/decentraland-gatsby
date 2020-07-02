df7657b7a2b6b83b1b803ece1eda16bbe2e0ba50
Fede <i@2fd.me>
2020-07-02 16:31:23 -0300

    chore: fix types

--- src/utils/array/chunk.test.ts
+++ src/utils/array/chunk.test.ts
@@ -1,12 +1,12 @@
-import chuck from "./chunk"
+import chunk from "./chunk"
 
 test('utils/array/chunk', () => {
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6]))).toEqual([[1], [2], [3], [4], [5], [6]])
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6], 2))).toEqual([[1, 2], [3, 4], [5, 6]])
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6], 3))).toEqual([[1, 2, 3], [4, 5, 6]])
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6], 4))).toEqual([[1, 2, 3, 4], [5, 6]])
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6], 5))).toEqual([[1, 2, 3, 4, 5], [6]])
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6], 6))).toEqual([[1, 2, 3, 4, 5, 6]])
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6], 7))).toEqual([[1, 2, 3, 4, 5, 6]])
-  expect(Array.from(chuck([1, 2, 3, 4, 5, 6], 10))).toEqual([[1, 2, 3, 4, 5, 6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6]))).toEqual([[1], [2], [3], [4], [5], [6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6], 2))).toEqual([[1, 2], [3, 4], [5, 6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6], 3))).toEqual([[1, 2, 3], [4, 5, 6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6], 4))).toEqual([[1, 2, 3, 4], [5, 6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6], 5))).toEqual([[1, 2, 3, 4, 5], [6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6], 6))).toEqual([[1, 2, 3, 4, 5, 6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6], 7))).toEqual([[1, 2, 3, 4, 5, 6]])
+  expect(Array.from(chunk([1, 2, 3, 4, 5, 6], 10))).toEqual([[1, 2, 3, 4, 5, 6]])
 })
