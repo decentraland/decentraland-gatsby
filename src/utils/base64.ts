c3307ee13b4ebb1a87a5209a2b879e30fb0a0ccd
Fede <i@2fd.me>
2020-07-13 14:00:33 -0300

    fix: support ArrayBuffer as param of toBase64 on web

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,6 +1,8 @@
 export function toBase64(original: string | ArrayBuffer) {
-  if (typeof btoa === 'function') {
+  if (typeof btoa === 'function' && typeof original === 'string') {
     return btoa(original as string)
+  } else if (typeof btoa === 'function') {
+    return btoa(String.fromCharCode(...new Uint8Array(original as ArrayBuffer)))
   } else if (typeof original === 'string') {
     return Buffer.from(original, 'utf8').toString('base64')
   } else {
