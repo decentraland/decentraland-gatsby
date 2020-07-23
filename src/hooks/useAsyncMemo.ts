19f60703bb9dda898fbced8b64509c8e635a0b76
Fede <i@2fd.me>
2020-07-23 11:30:00 -0300

    fix: improve useAsyncMemo

--- src/hooks/useAsyncMemo.ts
+++ src/hooks/useAsyncMemo.ts
@@ -3,6 +3,7 @@ import useAsyncEffect from './useAsyncEffect'
 
 type AsyncMemoState<T> = {
   version: number,
+  loading: boolean,
   value: T | null
 }
 
@@ -10,19 +11,22 @@ export default function useAsyncMemo<T>(exec: () => Promise<T>, deps: Dependency
 
   const [state, setState] = useState<AsyncMemoState<T>>({
     version: 0,
+    loading: false,
     value: null
   })
 
-  useAsyncEffect(async () => {
+  async function load() {
     const version = Math.ceil(Math.random() * 1e12)
     try {
-      setState((current) => ({ version, value: current.value }))
+      setState((current) => ({ version, value: current.value, loading: true }))
       const value = await exec()
-      setState((current) => current.version === version ? { version, value } : current)
+      setState((current) => current.version === version ? { version, value, loading: false } : current)
     } catch (err) {
       console.error(err)
     }
-  }, deps)
+  }
 
-  return state.value
+  useAsyncEffect(load, deps)
+
+  return [state.value, state.loading, load]
 }
