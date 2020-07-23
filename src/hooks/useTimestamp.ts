f2d8397c2ef8559ea5c0e0db6a7e72799874f087
Fede <i@2fd.me>
2020-07-23 12:27:32 -0300

    fix: useTimestamp types

--- src/hooks/useTimestamp.ts
+++ src/hooks/useTimestamp.ts
@@ -2,5 +2,5 @@ import { useState } from 'react';
 
 export default function useTimestamp() {
   const [state, setState] = useState(Date.now())
-  return [state, () => setState(Date.now())]
+  return [state, () => setState(Date.now())] as const
 }
