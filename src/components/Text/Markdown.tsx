fdfa22971df54e2573a53dc822f1ccd8380ee39d
Fede <i@2fd.me>
2020-07-27 13:38:21 -0300

    feat: support title and table for markdown

--- src/components/Text/Markdown.tsx
+++ src/components/Text/Markdown.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactMarkdown, { ReactMarkdownProps, NodeType } from 'react-markdown'
+import { Table } from 'decentraland-ui/dist/components/Table/Table'
+import SubTitle from './SubTitle';
 import Paragraph from './Paragraph';
 import Italic from './Italic';
 import Bold from './Bold';
@@ -9,12 +11,20 @@ import Link from './Link';
 export type MarkdownProps = Omit<ReactMarkdownProps, 'renders' | 'linkTarget' | 'astPlugins' | 'plugins'>
 
 export const renderers = {
+  heading: SubTitle,
   paragraph: Paragraph,
   emphasis: Italic,
   strong: Bold,
   link: Link,
   code: Code,
   inlineCode: Code,
+  table: (props: React.Props<any>) => <Table basic="very">{props.children}</Table>,
+  tableHead: (props: React.Props<any>) => <Table.Header>{props.children}</Table.Header>,
+  tableBody: (props: React.Props<any>) => <Table.Body>{props.children}</Table.Body>,
+  tableRow: (props: React.Props<any>) => <Table.Row>{props.children}</Table.Row>,
+  tableCell: (props: React.Props<any> & { isHeader: boolean, align?: 'center' | 'left' | 'right' }) => props.isHeader ?
+    <Table.HeaderCell textAlign={props.align || undefined}>{props.children}</Table.HeaderCell> :
+    <Table.Cell textAlign={props.align || undefined}>{props.children}</Table.Cell>,
 }
 
 export const allowedTypes = ['root', 'text'].concat(Object.keys(renderers)) as NodeType[]
