b419e3adec5ae842522e2f1f233afad01305362b
Fede <i@2fd.me>
2020-08-03 08:20:16 -0300

    feat: add news domains

--- src/entities/Route/types.ts
+++ src/entities/Route/types.ts
@@ -53,6 +53,8 @@ export const defaultOrigin = [
   /https:\/\/([a-zA-Z0-9\-_]+\.)*decentraland\.today/,
   /https:\/\/([a-zA-Z0-9\-_]+\.)*decentraland\.zone/,
   /https:\/\/([a-zA-Z0-9\-_]+\.)*decentraland\.org/,
+  /https:\/\/([a-zA-Z0-9\-_]+\.)*decentraland\.net/,
+  /https:\/\/([a-zA-Z0-9\-_]+\.)*decentraland\.co/,
   /https:\/\/([a-zA-Z0-9\-_]+\.)*decentraland\.io/,
   /https:\/\/([a-zA-Z0-9\-_]+\.)*now\.sh/,
   /https:\/\/([a-zA-Z0-9\-_]+\.)*manaland\.cn/,
