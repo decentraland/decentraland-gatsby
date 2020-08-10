8bd31fa6cbb8ec02705968cbbf170a7154f3953d
Fede <i@2fd.me>
2020-08-10 18:16:48 -0300

    fix: improve file reading

--- src/entities/Route/routes.ts
+++ src/entities/Route/routes.ts
@@ -39,16 +39,19 @@ export function cors(options: CorsOptions = {}) {
 }
 
 export function file(path: string, status: number = 200) {
-  let data: Buffer | null = null
-  let etag: string | null = null
+  let reader: Promise<readonly [Buffer, string]> | null = null
   return middleware(async (req, res: Response) => {
-    if (!data) {
-      data = await promisify(readFile)(path)
-      const hash = createHash('sha256')
-      hash.write(data)
-      etag = hash.digest('hex')
+    if (!reader) {
+      reader = (async () => {
+        const data = await promisify(readFile)(path).catch(() => Buffer.alloc(0))
+        const hash = createHash('sha256')
+        hash.write(data)
+        const etag = hash.digest('hex')
+        return [data, etag] as const
+      })()
     }
 
+    const [data, etag] = await reader
     res
       .status(status)
       .set('cache-control', 'public, max-age=86400')
