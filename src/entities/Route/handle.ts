943509f64f367524dbe50401140c0632c17f04d1
Fede <i@2fd.me>
2020-08-26 14:03:14 -0300

    chore: improve error logs

--- src/entities/Route/handle.ts
+++ src/entities/Route/handle.ts
@@ -48,7 +48,7 @@ function handleResponseError(req: Request, res: Response, err: RequestError) {
     body: (req as any).body,
   }
 
-  console.error(`Error executing request ${req.method} ${req.path} : `, data);
+  console.error(`Error executing request ${req.method} ${req.path} : `, JSON.stringify(data));
 
   if (!res.headersSent) {
     res.status(err.statusCode || RequestError.InternalServerError)
