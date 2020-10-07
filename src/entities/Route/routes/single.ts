c9648fe2f8abed37c2bc5e7d43bffe95cd883783
Fede <i@2fd.me>
2020-10-07 14:13:47 -0300

    feat: add `entities/Route/routes/single`

---
+++ src/entities/Route/routes/single.ts
@@ -0,0 +1,24 @@
+import handle, { AsyncHandler } from '../handle';
+
+export default function single(handler: AsyncHandler) {
+  let loaders = new Map<string, Promise<any>>()
+
+  return handle(async (req, res, ctx) => {
+    if (loaders.has(req.path)) {
+      return loaders.get(req.path)
+    }
+
+    const loader = handler(req, res, ctx)
+      .then((response: any) => {
+        loaders.delete(req.path)
+        return response
+      })
+      .catch((err: Error) => {
+        loaders.delete(req.path)
+        return Promise.reject(err)
+      })
+
+    loaders.set(req.path, loader)
+    return loader
+  })
+}
