2d9954722f9709023f87ed10e69f51ca22a4ccd9
Fede <i@2fd.me>
2020-10-20 01:20:37 -0300

    fix: add return type to `validate` function

--- src/entities/Route/validate.ts
+++ src/entities/Route/validate.ts
@@ -1,13 +1,15 @@
 import Ajv from 'ajv'
 import RequestError from './error'
 
-export default function validate(validator: Ajv.ValidateFunction, body: Record<string, any>) {
+export default function validate<R extends {}>(validator: Ajv.ValidateFunction, body: Record<string, any> = {}): R {
   if (validator(body) && validator.errors && validator.errors.length > 0) {
 
     const errors = validator.errors
     .map((error) => `${error.dataPath.slice(1)} ${error.message!}`)
     .filter(Boolean)
 
-    new RequestError('Invalid event data', RequestError.BadRequest, { errors, body })
+    throw new RequestError('Invalid event data', RequestError.BadRequest, { errors, body })
   }
+
+  return body as R
 }
