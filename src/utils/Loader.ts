ddc9e29afb54448683f95ba0044bf6a71d3d86fc
Fede <i@2fd.me>
2020-10-23 02:08:37 -0300

    fix: collect data on Loader

--- src/utils/Loader.ts
+++ src/utils/Loader.ts
@@ -1,12 +1,20 @@
 export default class Loader<T> {
   
   cache: Map<string, Promise<T>> = new Map()
+  data: Map<string, T> = new Map()
 
   constructor(private loader: (key: string) => Promise<T>) {}
 
   async load(key: string): Promise<T> {
     if (!this.cache.has(key)) {
-      this.cache.set(key, this.loader(key))
+      this.cache.set(
+        key,
+        this.loader(key)
+          .then(result => {
+            this.data.set(key, result)
+            return result
+          })
+      )
     }
     
     return this.cache.get(key)!
