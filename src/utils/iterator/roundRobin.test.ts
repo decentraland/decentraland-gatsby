282f35d4eee99278d9f19f30f7fc2ad725439a61
Fede <i@2fd.me>
2020-10-28 18:14:30 -0300

    feat: add utils/iterator/roundRobin

---
+++ src/utils/iterator/roundRobin.test.ts
@@ -0,0 +1,23 @@
+import roundRobin from "./roundRobin"
+
+test('utils/iterator/roundRobin', () => {
+  const randomList = [
+    Math.random(),
+    Math.random(),
+    Math.random(),
+  ]
+  const getRandom = roundRobin(randomList)
+
+  expect(getRandom()).toEqual(randomList[0])
+  expect(getRandom()).toEqual(randomList[1])
+  expect(getRandom()).toEqual(randomList[2])
+  expect(getRandom()).toEqual(randomList[0])
+  expect(getRandom()).toEqual(randomList[1])
+  expect(getRandom()).toEqual(randomList[2])
+  expect(getRandom()).toEqual(randomList[0])
+  expect(getRandom()).toEqual(randomList[1])
+  expect(getRandom()).toEqual(randomList[2])
+  expect(getRandom()).toEqual(randomList[0])
+  expect(getRandom()).toEqual(randomList[1])
+  expect(getRandom()).toEqual(randomList[2])
+})
---
+++ src/utils/iterator/roundRobin.ts
@@ -0,0 +1,13 @@
+export default function roundRobin<T>(items:T[]) {
+  if (items.length === 0) {
+    throw new Error(`Round Robin required at least 1 item`)
+  }
+
+  let current = 0
+  return function roundRobinGetter() {
+    const result = items[current]
+    current = current === items.length - 1 ? 0 : current + 1
+    return result
+  }
+}
+
