2ad761493a95ba3b7a6f15f32211e5d6042b35d2
Fede <i@2fd.me>
2020-11-16 01:29:25 -0300

    fix: add indexFile option to filesystem handle

--- src/entities/Route/routes/filesystem.ts
+++ src/entities/Route/routes/filesystem.ts
@@ -5,27 +5,32 @@ import redirect from './redirect'
 import file from './file'
 
 export type FilesystemHandleOptions = {
-  notFoundPage: string
+  indexFile: string,
+  notFoundFile: string,
 }
 
-function filesystemOptions(options: string | FilesystemHandleOptions) {
-  if (typeof options === 'string') {
-    return { notFoundPage: options }
+function filesystemOptions(value: string | Partial<FilesystemHandleOptions>): FilesystemHandleOptions {
+
+  const options = typeof value === 'string' ? { notFoundFile: value } : value
+
+  return {
+    indexFile: 'index.html',
+    notFoundFile: '404.html',
+    ...options
   }
-
-  return options
 }
 
-export default function filesystem(path: string, notFoundPage: string | FilesystemHandleOptions) {
+export default function filesystem(path: string, notFoundPage: string | Partial<FilesystemHandleOptions>) {
   const router = Router()
   const options = filesystemOptions(notFoundPage)
+  const indexFile = '/' + options.indexFile
   const cwd = resolve(process.cwd(), path)
   const files = new Set(glob.sync('**/*', { cwd, nodir: true }))
 
   for (const filepath of files.values()) {
     const webpath = '/' + filepath // => /en/index.html
 
-    if (webpath.endsWith('/index.html')) {
+    if (webpath.endsWith(indexFile)) {
       const basepath = webpath.slice(0, -10)
       router.get(webpath, redirect(basepath)) // redirect /en/index.html => /en/
       router.get(basepath, file(resolve(cwd, filepath))) // load /en/index.html on /en/
@@ -34,6 +39,6 @@ export default function filesystem(path: string, notFoundPage: string | Filesyst
     }
   }
 
-  router.use(file(resolve(cwd, options.notFoundPage), 404))
+  router.use(file(resolve(cwd, options.notFoundFile), 404))
   return router
 }
