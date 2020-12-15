92c0b044091b7ce3e816113a3e088f0cfae77ec3
Fede <i@2fd.me>
2020-12-15 15:59:59 -0300

    fix: upgrade default server

--- bin/templates/src/server.ts
+++ bin/templates/src/server.ts
@@ -1,22 +1,32 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import { createReadStream } from 'fs'
+// import manager from 'decentraland-gatsby/dist/entities/Job/index'
 import { listen } from 'decentraland-gatsby/dist/entities/Server/utils'
-import { status, logger } from 'decentraland-gatsby/dist/entities/Route/routes'
+import { status, logger, filesystem, ddos } from 'decentraland-gatsby/dist/entities/Route/routes'
+import handle from 'decentraland-gatsby/dist/entities/Route/handle'
+import RequestError from 'decentraland-gatsby/dist/entities/Route/error'
+
+// const jobs = manager()
+// jobs.cron('@eachMinute', () => console.log('Runnign Job...'))
 
 const app = express()
 
+app.set('x-powered-by', false)
 app.use('/api', [
   status(),
   logger(),
+  ddos(),
   bodyParser.json(),
+  handle(async () => {
+    throw new RequestError('NotFound', RequestError.NotFound)
+  })
 ])
 
-app.use(express.static('public', { maxAge: 1000 * 60 * 60 }))
-app.use((_, res) => createReadStream(process.cwd() + '/public/404.html').pipe(res))
+app.use(filesystem('public', '404.html'))
 
 Promise.resolve()
   // .then(() => database.connect())
+  //.then(() => process.env.JOBS !== 'false' && jobs.start())
   .then(() => listen(
     app,
     process.env.PORT || 4000,
