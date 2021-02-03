244ac731093949b9b0cb24dfbda79b5a8f5c5a25
Fede <i@2fd.me>
2021-02-03 13:15:50 -0300

    feat: add Avatar component

---
+++ src/components/Profile/Avatar.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import useAsyncMemo from '../../hooks/useAsyncMemo'
+import { StyleNamespace } from '../../variables'
+import TokenList from '../../utils/TokenList'
+import profiles from '../../utils/store/profiles'
+
+import './Avatar.css'
+
+const DEFAULT_AVATAR = 'https://peer.decentraland.org/content/contents/QmVfTSvEJBtmaHrnmZtpoVCSpv2J69M1wqA97dc53DgbWG'
+
+export type AvatarProps = Omit<React.HTMLProps<HTMLImageElement>, 'height' | 'width' | 'size'> & {
+  address?: string,
+  size?: 'mini' | 'tiny' | 'small' | 'medium' | 'large' | 'big' | 'huge' | 'massive' | 'full'
+}
+
+export default React.memo(function Avatar({ address, size, src, ...props }: AvatarProps) {
+
+  const [ avatar ] = useAsyncMemo(() => profiles.load(address || ''), [ address ])
+
+  return <img
+    loading="lazy"
+    {...props as any}
+    src={avatar?.avatar?.snapshots?.face || DEFAULT_AVATAR}
+    width="128"
+    height="128"
+    className={TokenList.join([
+      StyleNamespace,
+      'dcl-avatar',
+      `dcl-avatar--${size}`,
+      `dcl-avatar--${((address || '')[2] || '').toLowerCase()}`,
+      true && `dcl-avatar--loading`,
+      props.className
+    ])}
+  />
+})
--- src/components/Profile/index.stories.tsx
+++ src/components/Profile/index.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from "@storybook/react";
 import centered from '@storybook/addon-centered/react'
 import Divider from '../Text/Divider';
-import ImgAvatar from './ImgAvatar'
+import Avatar from './Avatar'
 import Title from '../Text/Title';
 import SubTitle from '../Text/SubTitle';
 import Code from '../Text/Code';
@@ -12,30 +12,30 @@ import { Button } from 'decentraland-ui/dist/components/Button/Button';
 
 storiesOf('Profile', module)
   .addDecorator(centered)
-  .add('ImgAvatar', () => {
+  .add('Avatar', () => {
     return <Container>
       <Divider />
-      <Title><Code inline>ImgAvatar</Code> component:  </Title>
+      <Title><Code inline>Avatar</Code> component:  </Title>
       <Paragraph secondary>Allow you to show an image for a profile</Paragraph>
       <Divider line />
       <SubTitle>Automatic colors</SubTitle>
       <Paragraph>Background colors are automatically defined using the address </Paragraph>
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x0...'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x1...'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x2...'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x3...'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x4...'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x5..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x6..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x7..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x8..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0x9..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0xa..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0xb..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0xc..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0xd..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0xe..'} />
-      <ImgAvatar style={{ marginRight: '1rem' }} address={'0xf..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x0...'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x1...'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x2...'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x3...'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x4...'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x5..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x6..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x7..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x8..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0x9..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0xa..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0xb..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0xc..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0xd..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0xe..'} />
+      <Avatar style={{ marginRight: '1rem' }} address={'0xf..'} />
       <Divider line />
       <SubTitle>Sizes</SubTitle>
       <Paragraph>There are a few size you can use:</Paragraph>
@@ -43,59 +43,59 @@ storiesOf('Profile', module)
         {"'mini' | 'tiny' |  'small' | 'medium' | 'large' | 'big' | 'huge' | 'massive' | 'full'"}
       </Code>
       <Divider size="mini" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="mini" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="tiny" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="small" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="medium" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="large" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="big" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="huge" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="massive" />
-      <ImgAvatar style={{ marginRight: '1rem' }} size="full" />
+      <Avatar style={{ marginRight: '1rem' }} size="mini" />
+      <Avatar style={{ marginRight: '1rem' }} size="tiny" />
+      <Avatar style={{ marginRight: '1rem' }} size="small" />
+      <Avatar style={{ marginRight: '1rem' }} size="medium" />
+      <Avatar style={{ marginRight: '1rem' }} size="large" />
+      <Avatar style={{ marginRight: '1rem' }} size="big" />
+      <Avatar style={{ marginRight: '1rem' }} size="huge" />
+      <Avatar style={{ marginRight: '1rem' }} size="massive" />
+      <Avatar style={{ marginRight: '1rem' }} size="full" />
       <Divider size="small" />
       <Paragraph>You can used beside <Code inline>Button</Code>s </Paragraph>
       <div>
-        <ImgAvatar style={{}} size="mini" />
+        <Avatar style={{}} size="mini" />
         <Button basic size="mini">mini button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="mini" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="mini" />
         <Button primary size="mini">mini button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="mini" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="mini" />
         <Button inverted primary size="mini">mini button</Button>
       </div>
       <Divider size="tiny" />
       <div>
-        <ImgAvatar style={{}} size="tiny" />
+        <Avatar style={{}} size="tiny" />
         <Button basic size="tiny">tiny button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="tiny" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="tiny" />
         <Button primary size="tiny">tiny button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="tiny" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="tiny" />
         <Button inverted primary size="tiny">tiny button</Button>
       </div>
       <Divider size="tiny" />
       <div>
-        <ImgAvatar style={{}} size="small" />
+        <Avatar style={{}} size="small" />
         <Button basic size="small">small button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="small" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="small" />
         <Button primary size="small">small button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="small" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="small" />
         <Button inverted primary size="small">small button</Button>
       </div>
       <Divider size="tiny" />
       <div>
-        <ImgAvatar style={{}} size="medium" />
+        <Avatar style={{}} size="medium" />
         <Button basic size="medium">medium button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="medium" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="medium" />
         <Button primary size="medium">medium button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="medium" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="medium" />
         <Button inverted primary size="medium">medium button</Button>
       </div>
       <Divider size="tiny" />
       <div>
-        <ImgAvatar style={{}} size="large" />
+        <Avatar style={{}} size="large" />
         <Button basic size="large">large button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="large" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="large" />
         <Button primary size="large">large button</Button>
-        <ImgAvatar style={{ margin: '0 .5rem 0 2rem' }} size="large" />
+        <Avatar style={{ margin: '0 .5rem 0 2rem' }} size="large" />
         <Button inverted primary size="large">large button</Button>
       </div>
       <Divider />
