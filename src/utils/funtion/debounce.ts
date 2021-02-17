5c8ad804c32cdb49db668f0ad8221c337e7a6ac5
Fede <i@2fd.me>
2021-02-17 15:08:19 -0300

    feat: add `utils/function/deboune`

---
+++ src/utils/funtion/debounce.ts
@@ -0,0 +1,13 @@
+export type Callback = (...args: any) => void
+
+export default function debounce<C extends Callback>(callback: C, timeout: number = 300): C {
+  let timer: null | ReturnType<typeof setTimeout> = null
+
+  return function debounceInstance(...args: any[]) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+
+    timer = setTimeout(() => callback(...args), timeout)
+  } as C
+}
