2ecb2eb3cd23b7dd24b35e7c2f5eee83466e4ea1
Fede <i@2fd.me>
2021-02-19 03:33:39 -0300

    refactor!: useFormatMessage
    BREAKING CHANGE: `useFormatMessage` was moved out of `utils/intl`
    BREAKING CHANGE: `utils/intl` was moved to `utils/react/intl`
    BREAKING CHANGE: use `l.str` as default function
    BREAKING CHANGE: default function `l` was moved to `l.markdown`
    BREAKING CHANGE: function `l.empty` was renamed to `l.isEmpty`
    BREAKING CHANGE: drop `l.iter`, use `utils/array/iter` instead

---
+++ src/utils/react/intl.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { IntlShape } from "gatsby-plugin-intl"
+import Markdown from "../../components/Text/Markdown"
+
+export function createFormatMessage(shape: IntlShape) {
+  const isEmpty = (id: string) => {
+    return !shape.messages[id]
+  }
+
+  const str = (id: string, values?: any) => {
+    if (isEmpty(id)) {
+      return null
+    }
+
+    return shape.formatMessage({ id }, { ...values })
+  }
+
+  const markdown = (id: string, values?: any) => {
+
+    let message = str(id, values)
+
+    if (typeof message !== 'string') {
+      return message
+    }
+
+    return <Markdown key={id} source={message} />
+  };
+
+  return Object.assign(str, { markdown, isEmpty })
+}
