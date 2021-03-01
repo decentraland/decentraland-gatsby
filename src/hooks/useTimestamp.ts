1dd93f38341c6b723555c3795cce12efe9a18b01
Fede <i@2fd.me>
2021-03-01 12:37:09 -0300

    refactor!: drop useTimestamp

--- src/hooks/useTimestamp.ts
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-
-export default function useTimestamp() {
-  const [state, setState] = useState(Date.now())
-  return [state, () => setState(Date.now())] as const
-}
