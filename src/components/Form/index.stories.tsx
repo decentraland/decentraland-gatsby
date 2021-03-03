2255e9cbd4060afcb2883a8fb9ed9cd770319b30
Fede <i@2fd.me>
2021-03-03 16:21:55 -0300

    docs: hide old documentation

--- src/components/Form/index.stories.tsx
+++ src/components/Form/index.stories.tsx
@@ -1,149 +1,149 @@
-import React from 'react'
-import { storiesOf } from "@storybook/react";
-import centered from '@storybook/addon-centered/react'
-import Divider from '../Text/Divider';
-import Title from '../Text/Title';
-import SubTitle from '../Text/SubTitle';
-import Code from '../Text/Code';
-import { Container } from 'decentraland-ui/dist/components/Container/Container';
-import Paragraph from '../Text/Paragraph';
-import { Button } from 'decentraland-ui/dist/components/Button/Button';
-import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid/Grid';
-import Input from './Input'
-import Subscribe from './Subscribe'
+// import React from 'react'
+// import { storiesOf } from "@storybook/react";
 
-storiesOf('From', module)
-  .addDecorator(centered)
-  .add('Input', () => {
-    return <Container>
-      <Divider />
-      <Title><Code inline>Input</Code> component:  </Title>
-      <Divider line />
-      <SubTitle>Common attributes</SubTitle>
-      <Paragraph secondary> <Code inline>placeholder</Code>, <Code inline>value</Code>, <Code inline>disabled</Code>   </Paragraph>
-      <Input verticalAlign="top" placeholder="with placeholder" defaultValue="" />{' '}
-      <Input verticalAlign="top" placeholder="" defaultValue="with value" />{' '}
-      <Input verticalAlign="top" placeholder="with error" defaultValue="" error />{' '}
-      <Input verticalAlign="top" placeholder="placeholder" defaultValue="" message="with message" />{' '}
-      <Input verticalAlign="top" placeholder="placeholder" defaultValue="" error message="with error message" />{' '}
-      <Input verticalAlign="top" placeholder="disabled" defaultValue="" disabled />{' '}
-      <Divider line />
+// import Divider from '../Text/Divider';
+// import Title from '../Text/Title';
+// import SubTitle from '../Text/SubTitle';
+// import Code from '../Text/Code';
+// import { Container } from 'decentraland-ui/dist/components/Container/Container';
+// import Paragraph from '../Text/Paragraph';
+// import { Button } from 'decentraland-ui/dist/components/Button/Button';
+// import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid/Grid';
+// import Input from './Input'
+// import Subscribe from './Subscribe'
 
-      <SubTitle>Sizes</SubTitle>
-      <Divider size="mini" />
-      <Input verticalAlign="top" size="mini" placeholder="mini" /> {' '}
-      <Input verticalAlign="top" size="tiny" placeholder="tiny" /> {' '}
-      <Input verticalAlign="top" size="small" placeholder="small" /> {' '}
-      <Input verticalAlign="top" size="medium" placeholder="medium" /> {' '}
-      <Input verticalAlign="top" size="large" placeholder="large" /> {' '}
-      <Divider line />
+// storiesOf('From', module)
 
-      <SubTitle>With Buttons</SubTitle>
-      <Paragraph>You can use this input with buttons there size are the same</Paragraph>
-      <Divider size="mini" />
-      <Grid stackable padded relaxed divided="vertically">
-        <Grid.Row>
-          <Grid.Column tablet="5">
-            <Input size="mini" placeholder="mini" /> {' '}
-            <Button size="mini" primary>mini</Button>
-          </Grid.Column>
-          <Grid.Column tablet="5">
-            <Input size="mini" placeholder="mini" /> {' '}
-            <Button size="mini" primary inverted>mini</Button>
-          </Grid.Column>
-          <Grid.Column tablet="5">
-            <Input size="mini" placeholder="mini" /> {' '}
-            <Button size="mini" basic>mini</Button>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column tablet="5">
-            <Input size="tiny" placeholder="tiny" /> {' '}
-            <Button size="tiny" primary>tiny</Button>
-          </Grid.Column>
-          <Grid.Column tablet="5">
-            <Input size="tiny" placeholder="tiny" /> {' '}
-            <Button size="tiny" primary inverted>tiny</Button>
-          </Grid.Column>
-          <Grid.Column tablet="5">
-            <Input size="tiny" placeholder="tiny" /> {' '}
-            <Button size="tiny" basic>tiny</Button>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column tablet="8">
-            <Input size="small" placeholder="small" /> {' '}
-            <Button size="small" primary>small</Button>
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Input size="small" placeholder="small" /> {' '}
-            <Button size="small" primary inverted>small</Button>
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Input size="small" placeholder="small" /> {' '}
-            <Button size="small" basic>small</Button>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column tablet="8">
-            <Input size="medium" placeholder="medium" /> {' '}
-            <Button size="medium" primary>medium</Button>
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Input size="medium" placeholder="medium" /> {' '}
-            <Button size="medium" primary inverted>medium</Button>
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Input size="medium" placeholder="medium" /> {' '}
-            <Button size="medium" basic>medium</Button>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column tablet="8">
-            <Input size="large" placeholder="large" /> {' '}
-            <Button size="large" primary>large</Button>
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Input size="large" placeholder="large" /> {' '}
-            <Button size="large" primary inverted>large</Button>
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Input size="large" placeholder="large" /> {' '}
-            <Button size="large" basic>large</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      <Divider />
-    </Container>
-  })
-  .add('Subscribe', () => {
-    return <Container>
-      <Divider />
-      <Title><Code inline>Subscribe</Code> component:  </Title>
-      <Divider line />
-      <SubTitle>Attributes</SubTitle>
-      <Grid stackable padded relaxed divided="vertically">
-        <Grid.Row>
-          <Grid.Column tablet="8">
-            <Subscribe primary />
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Subscribe inverted />
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Subscribe primary inverted />
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Subscribe basic />
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Subscribe primary loading />
-          </Grid.Column>
-          <Grid.Column tablet="8">
-            <Subscribe primary disabled />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      <Divider />
-    </Container>
-  })
+//   .add('Input', () => {
+//     return <Container>
+//       <Divider />
+//       <Title><Code inline>Input</Code> component:  </Title>
+//       <Divider line />
+//       <SubTitle>Common attributes</SubTitle>
+//       <Paragraph secondary> <Code inline>placeholder</Code>, <Code inline>value</Code>, <Code inline>disabled</Code>   </Paragraph>
+//       <Input verticalAlign="top" placeholder="with placeholder" defaultValue="" />{' '}
+//       <Input verticalAlign="top" placeholder="" defaultValue="with value" />{' '}
+//       <Input verticalAlign="top" placeholder="with error" defaultValue="" error />{' '}
+//       <Input verticalAlign="top" placeholder="placeholder" defaultValue="" message="with message" />{' '}
+//       <Input verticalAlign="top" placeholder="placeholder" defaultValue="" error message="with error message" />{' '}
+//       <Input verticalAlign="top" placeholder="disabled" defaultValue="" disabled />{' '}
+//       <Divider line />
+
+//       <SubTitle>Sizes</SubTitle>
+//       <Divider size="mini" />
+//       <Input verticalAlign="top" size="mini" placeholder="mini" /> {' '}
+//       <Input verticalAlign="top" size="tiny" placeholder="tiny" /> {' '}
+//       <Input verticalAlign="top" size="small" placeholder="small" /> {' '}
+//       <Input verticalAlign="top" size="medium" placeholder="medium" /> {' '}
+//       <Input verticalAlign="top" size="large" placeholder="large" /> {' '}
+//       <Divider line />
+
+//       <SubTitle>With Buttons</SubTitle>
+//       <Paragraph>You can use this input with buttons there size are the same</Paragraph>
+//       <Divider size="mini" />
+//       <Grid stackable padded relaxed divided="vertically">
+//         <Grid.Row>
+//           <Grid.Column tablet="5">
+//             <Input size="mini" placeholder="mini" /> {' '}
+//             <Button size="mini" primary>mini</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="5">
+//             <Input size="mini" placeholder="mini" /> {' '}
+//             <Button size="mini" primary inverted>mini</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="5">
+//             <Input size="mini" placeholder="mini" /> {' '}
+//             <Button size="mini" basic>mini</Button>
+//           </Grid.Column>
+//         </Grid.Row>
+//         <Grid.Row>
+//           <Grid.Column tablet="5">
+//             <Input size="tiny" placeholder="tiny" /> {' '}
+//             <Button size="tiny" primary>tiny</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="5">
+//             <Input size="tiny" placeholder="tiny" /> {' '}
+//             <Button size="tiny" primary inverted>tiny</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="5">
+//             <Input size="tiny" placeholder="tiny" /> {' '}
+//             <Button size="tiny" basic>tiny</Button>
+//           </Grid.Column>
+//         </Grid.Row>
+//         <Grid.Row>
+//           <Grid.Column tablet="8">
+//             <Input size="small" placeholder="small" /> {' '}
+//             <Button size="small" primary>small</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Input size="small" placeholder="small" /> {' '}
+//             <Button size="small" primary inverted>small</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Input size="small" placeholder="small" /> {' '}
+//             <Button size="small" basic>small</Button>
+//           </Grid.Column>
+//         </Grid.Row>
+//         <Grid.Row>
+//           <Grid.Column tablet="8">
+//             <Input size="medium" placeholder="medium" /> {' '}
+//             <Button size="medium" primary>medium</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Input size="medium" placeholder="medium" /> {' '}
+//             <Button size="medium" primary inverted>medium</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Input size="medium" placeholder="medium" /> {' '}
+//             <Button size="medium" basic>medium</Button>
+//           </Grid.Column>
+//         </Grid.Row>
+//         <Grid.Row>
+//           <Grid.Column tablet="8">
+//             <Input size="large" placeholder="large" /> {' '}
+//             <Button size="large" primary>large</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Input size="large" placeholder="large" /> {' '}
+//             <Button size="large" primary inverted>large</Button>
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Input size="large" placeholder="large" /> {' '}
+//             <Button size="large" basic>large</Button>
+//           </Grid.Column>
+//         </Grid.Row>
+//       </Grid>
+//       <Divider />
+//     </Container>
+//   })
+//   .add('Subscribe', () => {
+//     return <Container>
+//       <Divider />
+//       <Title><Code inline>Subscribe</Code> component:  </Title>
+//       <Divider line />
+//       <SubTitle>Attributes</SubTitle>
+//       <Grid stackable padded relaxed divided="vertically">
+//         <Grid.Row>
+//           <Grid.Column tablet="8">
+//             <Subscribe primary />
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Subscribe inverted />
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Subscribe primary inverted />
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Subscribe basic />
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Subscribe primary loading />
+//           </Grid.Column>
+//           <Grid.Column tablet="8">
+//             <Subscribe primary disabled />
+//           </Grid.Column>
+//         </Grid.Row>
+//       </Grid>
+//       <Divider />
+//     </Container>
+//   })
--- src/components/Image/index.stories.tsx
+++ src/components/Image/index.stories.tsx
@@ -1,76 +1,76 @@
-import React from 'react'
-import { storiesOf } from "@storybook/react";
-import centered from '@storybook/addon-centered/react'
-import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid'
-import Divider from '../Text/Divider';
-import ImgFixed from './ImgFixed'
-import Title from '../Text/Title';
-import Code from '../Text/Code';
-import { Container } from 'decentraland-ui/dist/components/Container/Container';
-import Paragraph from '../Text/Paragraph';
-import SubTitle from '../Text/SubTitle';
+// import React from 'react'
+// import { storiesOf } from "@storybook/react";
 
-storiesOf('Images', module)
-  .addDecorator(centered)
-  .add('ImgFixed', () => {
-    return <Container>
-      <Divider />
-      <Title><Code inline>ImgFixed</Code> component:  </Title>
-      <Paragraph secondary>Allow you insert an image with a constant relation between width and height</Paragraph>
-      <Divider size="small" line />
-      <SubTitle>Dimensions</SubTitle>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column mobile="4">
-            <Code inline>dimension="wide"</Code>
-            <ImgFixed dimension="wide" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-          <Grid.Column mobile="4">
-            <Code inline>dimension="square"</Code>
-            <ImgFixed dimension="square" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-          <Grid.Column mobile="4">
-            <Code inline>dimension="circle"</Code>
-            <ImgFixed dimension="circle" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-          <Grid.Column mobile="4">
-            <Code inline>dimension="vertical"</Code>
-            <ImgFixed dimension="vertical" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-        </Grid.Row >
-      </Grid >
-      <Divider size="small" line />
-      <SubTitle>Sizes</SubTitle>
-      <Grid>
-        <Grid.Row>
-          <Grid.Column mobile="5">
-            <Code inline>size="initial"</Code>
-            <ImgFixed size="initial" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-          <Grid.Column mobile="5">
-            <Code inline>size="cover"</Code>
-            <ImgFixed size="cover" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-          <Grid.Column mobile="5">
-            <Code inline>size="contain"</Code>
-            <ImgFixed size="contain" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column mobile="5">
-            <Code inline>size="75%"</Code>
-            <ImgFixed size="75%" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-          <Grid.Column mobile="5">
-            <Code inline>size="50%"</Code>
-            <ImgFixed size="50%" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-          <Grid.Column mobile="5">
-            <Code inline>size="25%"</Code>
-            <ImgFixed size="25%" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid >
-      <Divider />
-    </Container >
-  })
+// import Grid from 'semantic-ui-react/dist/commonjs/collections/Grid'
+// import Divider from '../Text/Divider';
+// import ImgFixed from './ImgFixed'
+// import Title from '../Text/Title';
+// import Code from '../Text/Code';
+// import { Container } from 'decentraland-ui/dist/components/Container/Container';
+// import Paragraph from '../Text/Paragraph';
+// import SubTitle from '../Text/SubTitle';
+
+// storiesOf('Images', module)
+
+//   .add('ImgFixed', () => {
+//     return <Container>
+//       <Divider />
+//       <Title><Code inline>ImgFixed</Code> component:  </Title>
+//       <Paragraph secondary>Allow you insert an image with a constant relation between width and height</Paragraph>
+//       <Divider size="small" line />
+//       <SubTitle>Dimensions</SubTitle>
+//       <Grid>
+//         <Grid.Row>
+//           <Grid.Column mobile="4">
+//             <Code inline>dimension="wide"</Code>
+//             <ImgFixed dimension="wide" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//           <Grid.Column mobile="4">
+//             <Code inline>dimension="square"</Code>
+//             <ImgFixed dimension="square" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//           <Grid.Column mobile="4">
+//             <Code inline>dimension="circle"</Code>
+//             <ImgFixed dimension="circle" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//           <Grid.Column mobile="4">
+//             <Code inline>dimension="vertical"</Code>
+//             <ImgFixed dimension="vertical" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//         </Grid.Row >
+//       </Grid >
+//       <Divider size="small" line />
+//       <SubTitle>Sizes</SubTitle>
+//       <Grid>
+//         <Grid.Row>
+//           <Grid.Column mobile="5">
+//             <Code inline>size="initial"</Code>
+//             <ImgFixed size="initial" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//           <Grid.Column mobile="5">
+//             <Code inline>size="cover"</Code>
+//             <ImgFixed size="cover" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//           <Grid.Column mobile="5">
+//             <Code inline>size="contain"</Code>
+//             <ImgFixed size="contain" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//         </Grid.Row>
+//         <Grid.Row>
+//           <Grid.Column mobile="5">
+//             <Code inline>size="75%"</Code>
+//             <ImgFixed size="75%" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//           <Grid.Column mobile="5">
+//             <Code inline>size="50%"</Code>
+//             <ImgFixed size="50%" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//           <Grid.Column mobile="5">
+//             <Code inline>size="25%"</Code>
+//             <ImgFixed size="25%" dimension="wide" background="#222" src="https://user-images.githubusercontent.com/2781777/40743488-0927f342-6428-11e8-942d-3ca36269d7dc.png" />
+//           </Grid.Column>
+//         </Grid.Row>
+//       </Grid >
+//       <Divider />
+//     </Container >
+//   })
