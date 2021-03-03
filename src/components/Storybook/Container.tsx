f09c3aa85cf365073c725f405d6681451af531df
Fede <i@2fd.me>
2021-03-03 16:22:20 -0300

    feat: add Storybook components

---
+++ src/components/Storybook/Container.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import "./Container.css"
+
+export default function Container(props: React.HTMLProps<HTMLDivElement>) {
+  return <div className="StorybookContainer" {...props} />
+}
