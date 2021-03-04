793dd62dd8d32f1692f318481b9fd926e3a7c91e
Fede <i@2fd.me>
2021-03-04 03:30:21 -0300

    fix: export useFormatMessage as default

--- src/hooks/useFormatMessage.ts
+++ src/hooks/useFormatMessage.ts
@@ -2,7 +2,7 @@ import {useMemo} from 'react'
 import { useIntl } from "gatsby-plugin-intl"
 import { createFormatMessage } from '../utils/react/intl'
 
-export function useFormatMessage() {
+export default function useFormatMessage() {
   const intl = useIntl()
   return useMemo(() => createFormatMessage(intl), [intl])
 }
