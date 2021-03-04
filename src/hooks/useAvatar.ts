ae25309ac1230481f6d514b48fc272a7ed6fb684
Fede <i@2fd.me>
2021-03-04 01:22:40 -0300

    fix: rename useProfile to useAvatar

--- src/hooks/useAvatar.ts
+++ src/hooks/useAvatar.ts
@@ -2,7 +2,7 @@ import useAuth from './useAuth'
 import useAsyncMemo from './useAsyncMemo'
 import Catalyst from '../utils/api/Catalyst'
 
-export default function useProfile() {
+export default function useAvatar() {
   const [ account ] = useAuth()
   return useAsyncMemo(async () => !account ? null : Catalyst.get().getProfile(account), [ account ])
 }
