b239c6a52a3917f3381e1190d975aa876658e166
Fede <i@2fd.me>
2021-03-04 02:47:34 -0300

    fix: add DependencyList to useInterval

--- src/hooks/useInterval.tsx
+++ src/hooks/useInterval.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, DependencyList } from 'react';
 
-export default function useInterval<T>(fun: () => T, interval: number): T {
+export default function useInterval<T>(fun: () => T, interval: number, deps: DependencyList = []): T {
   const [ value, setValue ] = useState(fun())
 
   useEffect(() => {
     const timer = setInterval(() => setValue(fun()), interval)
     return () => clearInterval(timer)
-  }, [ interval ])
+  }, [ interval, ...deps ])
 
   return value
 }
