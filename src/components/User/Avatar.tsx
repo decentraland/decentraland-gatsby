4ac323e5ac0732d26d1418d6f8887c060b628f4e
Fede <i@2fd.me>
2021-03-05 15:30:13 -0300

    feat: create components/User, deprecated components/Profile

---
+++ src/components/User/Avatar.tsx
@@ -0,0 +1,46 @@
+import React, { useMemo, useState } from 'react'
+import useAsyncMemo from '../../hooks/useAsyncMemo'
+import { StyleNamespace } from '../../variables'
+import TokenList from '../../utils/dom/TokenList'
+import profiles from '../../utils/loader/profile'
+
+import './Avatar.css'
+
+const DEFAULT_AVATAR = 'https://peer.decentraland.org/content/contents/QmXo9BXGF9Tx6H3hNwMfdMsisogfVgpy1bRwxPQ2D4QhPW'
+
+export type AvatarProps = Omit<React.HTMLProps<HTMLImageElement>, 'height' | 'width' | 'size'> & {
+  address?: string,
+  size?: 'mini' | 'tiny' | 'small' | 'medium' | 'large' | 'big' | 'huge' | 'massive' | 'full'
+}
+
+export default React.memo(function Avatar({ address, size, src, ...props }: AvatarProps) {
+
+  const [ failed, setFailed ] = useState(false)
+  const [ avatar, { loading } ] = useAsyncMemo(() => profiles.load(address || ''), [ address ])
+  const target = useMemo(() => {
+    if (src) {
+      return src
+    } else  if (failed || !(avatar?.avatar?.snapshots?.face)) {
+      return DEFAULT_AVATAR
+    } else {
+      return avatar?.avatar?.snapshots?.face
+    }
+  }, [ avatar, failed ])
+
+  return <img
+    loading="lazy"
+    {...props as any}
+    src={target}
+    onError={() => setFailed(true)}
+    width="128"
+    height="128"
+    className={TokenList.join([
+      StyleNamespace,
+      'dcl-avatar',
+      `dcl-avatar--${size}`,
+      `dcl-avatar--${((address || '')[2] || '').toLowerCase()}`,
+      loading && `dcl-avatar--loading`,
+      props.className
+    ])}
+  />
+})
