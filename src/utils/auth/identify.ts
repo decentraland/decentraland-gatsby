f72465309b4e777c99de0e2743f34caa72593d91
Fede <i@2fd.me>
2021-03-05 21:55:52 -0300

    fix: use legacy provider

--- src/utils/auth/identify.ts
+++ src/utils/auth/identify.ts
@@ -7,6 +7,7 @@ const dependencies = once(async () => Promise.all([
   import('web3x/address'),
   import('web3x/account'),
   import('web3x/personal'),
+  import('web3x/providers'),
   import('web3x/utils'),
   import('dcl-crypto/dist/Authenticator'),
 ] as const))
@@ -21,6 +22,7 @@ export default async function identify(connection: ConnectionResponse) {
       { Address } /* from web3x/address */ ,
       { Account } /* from web3x/account */ ,
       { Personal } /* from web3x/personal */ ,
+      { LegacyProviderAdapter } /* from web3x/providers */ ,
       { bufferToHex } /* from web3x/utils */ ,
       { Authenticator } /* from dcl-crypto/dist/Authenticator */ ,
     ] = await dependencies()
@@ -39,7 +41,7 @@ export default async function identify(connection: ConnectionResponse) {
       address,
       payload,
       expiration,
-      message => new Personal(provider).sign(message, Address.fromString(address), '')
+      message => new Personal(new LegacyProviderAdapter(provider as any)).sign(message, Address.fromString(address), '')
     );
 
     return identity
@@ -54,6 +56,7 @@ export async function ownerAddress(authChain: AuthChain) {
     /*web3x/address*/,
     /*web3x/account*/,
     /*web3x/personal*/,
+    /*web3x/providers*/,
     /*web3x/utils*/,
     { Authenticator } /* from dcl-crypto/dist/Authenticator */,
   ] = await dependencies()
