0afe87fe6078b14948721f167ea21a2fe631a27f
Fede <i@2fd.me>
2021-03-10 17:49:28 -0300

    feat: add utils/loader/manaBalance

--- src/utils/loader/index.ts
+++ src/utils/loader/index.ts
@@ -1,3 +1,4 @@
 export { default as Loader } from './Loader'
 export { default as profile } from './profile'
+export { default as manaBalance } from './manaBalance'
 export * from './types'
---
+++ src/utils/loader/manaBalance.ts
@@ -0,0 +1,52 @@
+import { MANA_GRAPH_BY_CHAIN_ID } from 'decentraland-dapps/dist/lib/chainConfiguration'
+import isEthereumAddress from 'validator/lib/isEthereumAddress'
+import { fromWei } from 'web3x/utils/units'
+import Loader from './Loader'
+
+type ChainId = keyof typeof MANA_GRAPH_BY_CHAIN_ID
+
+const MANA_QUERY = `
+query ($address: String!) {
+  accounts(where: { id: $address }) {
+    id,
+    mana
+  }
+}
+`
+
+function createBalanceLoader(chainId: ChainId) {
+  return new Loader(async (address: string) => {
+    if (!isEthereumAddress(address)) {
+      return 0
+    }
+
+    try {
+      const response = await fetch(MANA_GRAPH_BY_CHAIN_ID[chainId], {
+        method: 'psot',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query: MANA_QUERY,
+          values: { address: address.toLowerCase() }
+        })
+      })
+
+      const body = await response.json()
+      const accounts = body?.data?.accounts || []
+      const account = accounts[0]
+      const mana = account?.mana || '0'
+      return parseFloat(fromWei(mana, 'ether'))
+    }  catch (err) {
+      console.error(err)
+      return 0
+    }
+  })
+}
+
+const cache = new Map<ChainId, Loader<number>>()
+
+export default function manaBalance(chainId: ChainId) {
+  if (!cache.has(chainId)) {
+    cache.set(chainId, createBalanceLoader(chainId))
+  }
+  return cache.get(chainId)!
+}
