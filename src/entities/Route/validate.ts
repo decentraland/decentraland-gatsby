4239d331ca918b16ab938876a2d9453a6570f325
Fede <i@2fd.me>
2021-03-26 10:55:30 -0400

    fix: validation flow

--- src/entities/Route/validate.ts
+++ src/entities/Route/validate.ts
@@ -2,8 +2,7 @@ import Ajv from 'ajv'
 import RequestError from './error'
 
 export default function validate<R extends {}>(validator: Ajv.ValidateFunction, body: Record<string, any> = {}): R {
-  if (validator(body) && validator.errors && validator.errors.length > 0) {
-
+  if (!validator(body) && validator.errors && validator.errors.length > 0) {
     const errors = validator.errors
     .map((error) => `${error.dataPath.slice(1)} ${error.message!}`)
     .filter(Boolean)
