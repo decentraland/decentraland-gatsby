98041c0126a42935079c72dcb770604e0a364629
Fede <i@2fd.me>
2021-04-02 08:47:54 -0400

    fix: add database metrics

---
+++ src/entities/Database/metrics.ts
@@ -0,0 +1,21 @@
+import client, { registerMetric } from '../Prometheus/metrics'
+
+export type JobLabels = Record<'job', string | number>
+
+export const database_pool_size = new client.Gauge({
+  name: 'database_pool_size',
+  help: 'The number of queries that are running at the same time',
+  registers: [],
+  labelNames: [ 'query' ],
+})
+
+export const database_duration_seconds = new client.Histogram({
+  name: 'database_duration_seconds',
+  help: 'The time (in seconds) it takes for a query to complete',
+  registers: [],
+  labelNames: [ 'query', 'error' ],
+  buckets: [ .005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10, 20, 30, 60 ],
+})
+
+registerMetric(database_pool_size)
+registerMetric(database_duration_seconds)
--- src/entities/Database/model.ts
+++ src/entities/Database/model.ts
@@ -1,28 +1,137 @@
-import { Model as BaseModel, SQLStatement } from "decentraland-server";
+import { createHash } from 'crypto'
+import { Model as BaseModel, OnConflict, PrimaryKey, QueryPart, SQLStatement } from "decentraland-server";
+import { database_duration_seconds, database_pool_size } from './metrics';
 
-export class Model<T> extends BaseModel<T> {
+function hash(...chucks: (string | number | (string | number)[])[]) {
+  const query = chucks.map(chuck => Array.isArray(chuck) ? chuck.join('::') : chuck).join(':::')
+  return createHash('sha1').update(query).digest('hex').slice(0, 6)
+}
 
-  static async query<U = any>(query: SQLStatement): Promise<U[]> {
-    try {
-      return super.query(query.text, query.values)
-    } catch (err) {
-      err.query = query.text
-      err.values = query.values
-      throw err
-    }
+async function messureWithParams<T>(exec: () => Promise<T>, params: { queryId: string, [key: string]: any }): Promise<T> {
+  database_pool_size.inc({ query: params.queryId })
+  const complete = database_duration_seconds.startTimer({ query: params.queryId })
+  try {
+    const result = await exec()
+    database_pool_size.dec({ query: params.queryId })
+    complete({ error: 0 })
+    return result
+  } catch (err) {
+    database_pool_size.dec({ query: params.queryId })
+    complete({ error: 1 })
+    Object.assign(err, params)
+    throw err
+  }
+}
+
+export class Model<T extends {}> extends BaseModel<T> {
+
+  static async find<U extends {} = any>(conditions?: Partial<U>, orderBy?: Partial<U>, extra?: string): Promise<U[]> {
+    const queryId = hash(
+      'find',
+      this.tableName,
+      Object.keys(conditions || {}),
+      Object.keys(orderBy || {}),
+      extra || ''
+    )
+
+    return messureWithParams(
+      () => super.find(conditions, orderBy, extra),
+      { queryId, conditions, orderBy, extra }
+    )
+  }
+
+  static findOne<U extends {} = any, P extends QueryPart = any>(primaryKey: PrimaryKey, orderBy?: Partial<P>): Promise<U | undefined>;
+  static findOne<U extends QueryPart = any, P extends QueryPart = any>(conditions: Partial<U>, orderBy?: Partial<P>): Promise<U | undefined>;
+  static async findOne<U extends QueryPart = any, P extends QueryPart = any>(conditions: PrimaryKey | Partial<U>, orderBy?: Partial<P>): Promise<U | undefined> {
+    const queryId = hash(
+      'findOne',
+      this.tableName,
+      typeof conditions === 'object' ? Object.keys(conditions) : conditions,
+      Object.keys(orderBy || {})
+    )
+    return messureWithParams(
+      () => super.findOne(conditions as PrimaryKey, orderBy),
+      { queryId, conditions, orderBy }
+    )
+  }
+
+  static async count<U extends QueryPart = any>(conditions: Partial<U>, extra?: string): Promise<number> {
+    const queryId = hash(
+      'count',
+      this.tableName,
+      Object.keys(conditions),
+      extra || ''
+    )
+
+    return messureWithParams(
+      () => super.count(conditions, extra),
+      { queryId, conditions, extra }
+    )
+  }
+
+  static async create<U extends QueryPart = any>(row: U): Promise<U> {
+    const queryId = hash('create', this.tableName)
+    return messureWithParams(
+      () => super.create(row),
+      { queryId, row }
+    )
+  }
+
+  static async upsert<U extends QueryPart = any>(row: U, onConflict?: OnConflict<U>): Promise<U> {
+    const queryId = hash('upsert', this.tableName)
+    return messureWithParams(
+      () => super.upsert(row),
+      { queryId, row, onConflict }
+    )
+  }
+
+
+  static async update<U extends QueryPart = any, P extends QueryPart = any>(changes: Partial<U>, conditions: Partial<P>): Promise<any> {
+    const queryId = hash(
+      'update',
+      this.tableName,
+      Object.values(changes),
+      Object.values(conditions)
+    )
+
+    return messureWithParams(
+      () => super.update(changes, conditions),
+      { queryId, changes, conditions }
+    )
+  }
+
+  static async delete<U extends QueryPart = any>(conditions: Partial<U>): Promise<any> {
+    const queryId = hash(
+      'update',
+      this.tableName,
+      Object.values(conditions)
+    )
+
+    return messureWithParams(
+      () => super.delete(conditions),
+      { queryId, conditions }
+    )
+  }
+
+  static async query<U extends {}= any>(query: SQLStatement): Promise<U[]> {
+    const queryId = hash('query', this.tableName, query.text)
+    return messureWithParams(
+      () => super.query(query.text, query.values),
+      { queryId, query: query.text, values: query.values }
+    )
   }
 
   /**
    * Execute a query and returns the number of row affected
    */
   static async rowCount(query: SQLStatement): Promise<number> {
-    try {
-      const result = await this.db.client.query(query)
-      return result.rowCount
-    } catch (err) {
-      err.query = query.text
-      err.values = query.values
-      throw err
-    }
+    const queryId = hash('rowCount', this.tableName, query.text)
+    return messureWithParams(
+      async () => {
+        const result = await this.db.client.query(query)
+        return result.rowCount
+      },
+      { queryId, query: query.text, values: query.values }
+    )
   }
 }
