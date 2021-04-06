e2158a80f7d2c65a79c76d2eeb062c6f5681e688
Fede <i@2fd.me>
2021-04-06 12:40:46 -0400

    feat: allow to use number as key in `Loader`s

--- src/utils/loader/Loader.ts
+++ src/utils/loader/Loader.ts
@@ -1,14 +1,14 @@
 export default class Loader<T> {
 
-  cache: Map<string, Promise<T>> = new Map()
-  data: Map<string, T> = new Map()
-  readonly handle: (key: string) => Promise<T>
+  cache: Map<string | number, Promise<T>> = new Map()
+  data: Map<string | number, T> = new Map()
+  readonly handle: (key: string | number) => Promise<T>
 
-  constructor(handle: (key: string) => Promise<T>) {
+  constructor(handle: (key: string | number) => Promise<T>) {
     this.handle = handle
   }
 
-  private async _handle(key: string): Promise<T> {
+  private async _handle(key: string | number): Promise<T> {
     return this.handle(key)
       .then(result => {
         this.data.set(key, result)
@@ -20,7 +20,7 @@ export default class Loader<T> {
       })
   }
 
-  async load(key: string): Promise<T> {
+  async load(key: string | number): Promise<T> {
     if (!this.cache.has(key)) {
       this.cache.set(key, this._handle(key))
     }
@@ -28,16 +28,16 @@ export default class Loader<T> {
     return this.cache.get(key)!
   }
 
-  isLoading(key: string) {
+  isLoading(key: string | number) {
     return this.cache.has(key) && !this.data.has(key)
   }
 
-  set(key: string, value: T) {
+  set(key: string | number, value: T) {
     this.cache.set(key, Promise.resolve(value))
     this.data.set(key, value)
   }
 
-  clear(key: string) {
+  clear(key: string | number) {
     this.data.delete(key)
     return this.cache.delete(key)
   }
