e17c2fcd83be8383a244ba7c2c2d792e1cd81c06
Fede <i@2fd.me>
2021-04-16 23:37:30 -0300

    feat: add useGlobalMemo

---
+++ src/hooks/useGlobalMemo.ts
@@ -0,0 +1,14 @@
+import { hash } from 'immutable'
+import { useMemo, DependencyList } from 'react'
+
+const globalMemo = new Map<string, any>()
+export default function useGlobalMemo<T>(callback: () => T, deps: DependencyList) {
+  return useMemo<T>(() => {
+    const id = deps.map(dep => hash(dep)).join('::')
+    if (!globalMemo.has(id)) {
+      globalMemo.set(id, callback())
+    }
+
+    return globalMemo.get(id)
+  }, deps)
+}
