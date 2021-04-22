b6739c307567a6c10ff4960f06ca2bae2bd2afcd
Fede <i@2fd.me>
2021-04-22 09:10:44 -0300

    feat: drop useAvatar

--- src/hooks/useAvatar.ts
+++
@@ -1,8 +0,0 @@
-import useAuth from './useAuth'
-import useAsyncMemo from './useAsyncMemo'
-import profile from '../utils/loader/profile'
-
-export default function useAvatar() {
-  const [ account ] = useAuth()
-  return useAsyncMemo(async () => !account ? null : profile.load(account), [ account ])
-}
