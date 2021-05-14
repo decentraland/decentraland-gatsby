8e392ba7aa8c63f672472453d7819c2b03c411c4
Fede <i@2fd.me>
2021-05-14 17:47:17 -0300

    feat: add promise/delay

---
+++ src/utils/promise/delay.ts
@@ -0,0 +1,9 @@
+export default async function delay(time: number): Promise<void>;
+export default async function delay<T>(time: number, value?: Promise<T>): Promise<T>;
+export default async function delay<T>(time: number, value?: T): Promise<T>;
+export default async function delay<T>(time: number, value?: T): Promise<T> {
+  return Promise.resolve()
+    .then(() => new Promise(
+      resolve => setTimeout(() => resolve(value as T), time)
+    ))
+}
