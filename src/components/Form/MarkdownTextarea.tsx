57b80eade7e9f2b29701b03d440b4b19658d60e8
Fede <i@2fd.me>
2021-05-17 02:21:45 -0300

    fix: MarkdownTextarea styles

--- src/components/Form/MarkdownTextarea.tsx
+++ src/components/Form/MarkdownTextarea.tsx
@@ -8,16 +8,17 @@ import './MarkdownTextarea.css'
 
 type MarkdownTextarea = TextareaProps & { preview?: boolean }
 
-export default function MarkdownTextarea({ preview, className, ...props}: MarkdownTextarea) {
+export default function MarkdownTextarea({ preview, label, className, ...props}: MarkdownTextarea) {
   const [ previewing, setPreviewing ] = useState(preview)
 
-  return <div className={TokenList.join(['MarkdownTextarea', className])}>
+  return <div className={TokenList.join(['dcl', 'field', props.error && 'error', 'MarkdownTextarea', className])}>
     <Radio toggle label="PREVIEW" checked={preview ?? previewing} onChange={() => setPreviewing(!previewing)} style={{ position: 'absolute', right: 0, top: 0 }} />
+    <Label>{props.label || ""} &nbsp; </Label>
     {!previewing && <Textarea {...props} />}
-    {previewing && <Label>{props.label || ' '}</Label>}
-    {previewing && <div style={{ minHeight: '72px', paddingTop: '4px', paddingBottom: '12px' }}>
+    {previewing && <div className="MarkdownTextarea__Preview" style={{ minHeight: (props.minHeight || 72) + 'px' }}>
       <Markdown source={props.value} />
     </div>}
+    {previewing && <p className="message"> {props.message} &nbsp; </p>}
   </div>
 
 }
