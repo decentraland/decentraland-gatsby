68711a6d871ad6f57e27c0686250308d1a16242e
Fede <i@2fd.me>
2021-05-20 15:08:24 -0300

    fix: types

--- src/context/Auth/useEthContext.ts
+++ src/context/Auth/useEthContext.ts
@@ -2,6 +2,6 @@ import useEth from "../../hooks/useEth";
 import useAuthContext from "./useAuthContext";
 
 export default function useEthContext() {
-  const [ _account, state ] = useAuthContext()
-  return useEth(state.provider)
+  const [ , { provider } ] = useAuthContext()
+  return useEth(provider)
 }
