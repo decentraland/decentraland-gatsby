ccb92a80c377a0f77ac9fe791c90e94c7fc58719
Fede <i@2fd.me>
2021-05-25 02:09:38 -0300

    feat: add retry

---
+++ src/utils/promise/retry.test.ts
@@ -0,0 +1,32 @@
+import retry from "./retry"
+
+
+describe('src/utils/promise', () => {
+  test(`retry`, async () => {
+    let i = 0
+
+    const complete = retry(3, async () => {
+      return ++i
+    })
+
+    await expect(complete).resolves.toBe(1)
+
+    const eventual = retry(3, async () => {
+      ++i
+      if (i !== 3) {
+        throw new Error(`error n: ${i}`)
+      } else {
+        return i
+      }
+    })
+
+    await expect(eventual).resolves.toBe(3)
+
+    const failed = retry(3, async () => {
+      ++i
+      throw new Error(`error n: ${i}`)
+    })
+
+    await expect(failed).rejects.toEqual(new Error(`error n: 6`))
+  })
+})
---
+++ src/utils/promise/retry.ts
@@ -0,0 +1,17 @@
+export default async function retry<T>(times: number, fun: () => Promise<T>): Promise<T> {
+  while (times > 0) {
+    try {
+      const result = await fun()
+      return result
+    } catch (err) {
+      console.error(err)
+      if(times === 1) {
+        throw err
+      }
+    }
+
+    times--
+  }
+
+  throw new Error(`Cannot resolve promise`)
+}
