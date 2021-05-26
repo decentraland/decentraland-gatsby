5b7fab94add5b23fa5e4d10965ca24070d86718a
Fede <i@2fd.me>
2021-05-26 12:14:55 -0300

    refactor: improve wallet detection

--- src/utils/segment/segment.ts
+++ src/utils/segment/segment.ts
@@ -12,19 +12,20 @@ let context: TrackContext | null = null
 export default function segment(tracker: Tracker) {
   if (window.analytics) {
     if (!context) {
+      const ethereum = window?.ethereum as any
       context = {
         mobile: isMobile(),
         wallet: (
-          (window?.ethereum as any)?.isMetaMask ? 'metamask' :
-          (window?.ethereum as any)?.isDapper ? 'dapper' :
-          (window?.ethereum as any)?.isCucumber ? 'cucumber' :
-          (window?.ethereum as any)?.isTrust ? 'trust' :
-          (window?.ethereum as any)?.isToshi ? 'coinbase' :
-          (window?.ethereum as any)?.isGoWallet ? 'goWallet' :
-          (window?.ethereum as any)?.isAlphaWallet ? 'alphaWallet' :
-          (window?.ethereum as any)?.isStatus ? 'status' :
-          (window?.ethereum as any) ? 'other' :
-          false
+          !ethereum ? 'none' :
+          ethereum?.isMetaMask ? 'metamask' :
+          ethereum?.isDapper ? 'dapper' :
+          ethereum?.isCucumber ? 'cucumber' :
+          ethereum?.isTrust ? 'trust' :
+          ethereum?.isToshi ? 'coinbase' :
+          ethereum?.isGoWallet ? 'goWallet' :
+          ethereum?.isAlphaWallet ? 'alphaWallet' :
+          ethereum?.isStatus ? 'status' :
+          'other'
         )
       }
     }
