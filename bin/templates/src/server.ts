fbba60f5475b3766881fdc43b6b1bbf61abc47ad
Fede <i@2fd.me>
2021-05-27 08:55:58 -0300

    fix: update templates

--- bin/templates/src/server.ts
+++ bin/templates/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 // import manager from 'decentraland-gatsby/dist/entities/Job/index'
 import { listen } from 'decentraland-gatsby/dist/entities/Server/utils'
 import { status, filesystem } from 'decentraland-gatsby/dist/entities/Route/routes'
-import { withDDosProtection, withLogs, withCors } from 'decentraland-gatsby/dist/entities/Route/middleware'
+import { withDDosProtection, withLogs, withCors, withBody } from 'decentraland-gatsby/dist/entities/Route/middleware'
 import metrics from 'decentraland-gatsby/dist/entities/Prometheus/routes'
 import handle from 'decentraland-gatsby/dist/entities/Route/handle'
 import RequestError from 'decentraland-gatsby/dist/entities/Route/error'
@@ -18,7 +18,7 @@ app.use('/api', [
   status(),
   withDDosProtection(),
   withCors(),
-  // routes
+  withBody(),
   handle(async () => {
     throw new RequestError('NotFound', RequestError.NotFound)
   })
@@ -29,7 +29,7 @@ app.use(filesystem('public', '404.html'))
 
 Promise.resolve()
   // .then(() => database.connect())
-  //.then(() => process.env.JOBS !== 'false' && jobs.start())
+  // .then(() => process.env.JOBS !== 'false' && jobs.start())
   .then(() => listen(
     app,
     process.env.PORT || 4000,
