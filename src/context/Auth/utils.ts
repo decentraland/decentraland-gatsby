331f93008701c754da3cc5603c8e393cca73bfa6
Fede <i@2fd.me>
2021-05-27 08:46:23 -0300

    fix: set chain id

--- src/context/Auth/utils.ts
+++ src/context/Auth/utils.ts
@@ -1,31 +1,21 @@
 import { ChainId } from '@dcl/schemas'
 
-const DEFAULT_CHAIN_ID: ChainId = Number(
+const CHAIN_ID: ChainId = Number(
+  process.env.GATSBY_CHAIN_ID ||
+  process.env.REACT_APP_CHAIN_ID ||
+  process.env.STORYBOOK_CHAIN_ID ||
+  process.env.CHAIN_ID ||
   process.env.GATSBY_DEFAULT_CHAIN_ID ||
+  process.env.REACT_APP_DEFAULT_CHAIN_ID ||
+  process.env.STORYBOOK_DEFAULT_CHAIN_ID ||
   process.env.DEFAULT_CHAIN_ID ||
-  String(ChainId.MATIC_MAINNET)
-)
-
-const SUPPORTED_CHAIN_IDS: Set<ChainId> = new Set((
-  process.env.GATBY_SUPPORTED_CHAIN_ID ||
-  process.env.SUPPORTED_CHAIN_ID ||
-  Object.values(ChainId).filter(chainId => Number.isFinite(chainId)).join(',')
-)
-  .split(',')
-  .map(n => Number(n))
+  String(ChainId.ETHEREUM_MAINNET)
 )
 
 export function getDefaultChainId(): ChainId {
-  return DEFAULT_CHAIN_ID
+  return CHAIN_ID
 }
 
-export function getCurrentChainId(): ChainId {
-  if (typeof window !== 'undefined' && window.ethereum) {
-    const injectedChainId = Number((window.ethereum as any).chainId)
-    if (injectedChainId && SUPPORTED_CHAIN_IDS.has(injectedChainId)) {
-      return injectedChainId
-    }
-  }
-
-  return DEFAULT_CHAIN_ID
+export function getChainId(): ChainId {
+  return CHAIN_ID
 }
--- src/hooks/useChainId.ts
+++ src/hooks/useChainId.ts
@@ -1,23 +1,5 @@
-import { useEffect, useState } from 'react'
-import type { ChainId } from '@dcl/schemas'
-import { getCurrentChainId, getDefaultChainId } from '../context/Auth/utils'
-
-let CURRENT_CHAIN_ID = getDefaultChainId()
-let DETECTED_CHAIN_ID: ChainId | null = null
+import { getChainId } from '../context/Auth/utils'
 
 export default function useChainId() {
-  const [ chainId, setChainId ] = useState(DETECTED_CHAIN_ID || CURRENT_CHAIN_ID)
-
-  // Detect injected chain id
-  useEffect(() => {
-    if (DETECTED_CHAIN_ID == null) {
-      DETECTED_CHAIN_ID = getCurrentChainId()
-    }
-
-    if (DETECTED_CHAIN_ID !== chainId) {
-      setChainId(DETECTED_CHAIN_ID)
-    }
-  }, [])
-
-  return chainId
+  return getChainId()
 }
