803916dc336347d6b2ebca51a13192b3d76f6e8d
Fede <i@2fd.me>
2021-06-08 12:26:15 -0300

    chore: add modules/locations to base project

---
+++ bin/templates/src/modules/locations.ts
@@ -0,0 +1,7 @@
+import API from 'decentraland-gatsby/dist/utils/api/API'
+
+const GATSBY_BASE_URL = process.env.GATSBY_BASE_URL || '/'
+
+export default {
+  home: () => API.url(GATSBY_BASE_URL, '/'),
+}
