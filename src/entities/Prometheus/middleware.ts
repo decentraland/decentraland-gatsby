4c9fcb6556ae1709bb43955ace29360ff8963d46
Fede <i@2fd.me>
2021-06-18 19:02:24 -0300

    feat: add withPrometheusToken middleware

---
+++ src/entities/Prometheus/middleware.ts
@@ -0,0 +1,15 @@
+import { middleware } from "../Route/handle"
+import env from '../../utils/env'
+import { withBearerToken } from '../Auth/middleware'
+
+const PROMETHEUS_BEARER_TOKEN = env('PROMETHEUS_BEARER_TOKEN', '')
+
+export function withPrometheusToken(options: Partial<{ optional: boolean }>) {
+  if (PROMETHEUS_BEARER_TOKEN) {
+    return withBearerToken([PROMETHEUS_BEARER_TOKEN])
+  } else if (options.optional) {
+    return middleware(async () => {})
+  } else {
+    throw new Error(`Missing environment variable PROMETHEUS_BEARER_TOKEN`)
+  }
+}
--- src/entities/Prometheus/routes.ts
+++ src/entities/Prometheus/routes.ts
@@ -1,30 +1,14 @@
-import { yellow } from 'colors/safe'
 import client from 'prom-client'
-import env from '../../utils/env'
-import { withBearerToken } from '../Auth/middleware'
 import { registry } from './metrics'
 import { handleRaw } from '../Route/handle'
 import routes from '../Route/routes'
+import { withPrometheusToken } from './middleware'
 
-const PROMETHEUS_BEARER_TOKEN = env('PROMETHEUS_BEARER_TOKEN', '')
 const PROMETHEUS_REGISTRIES = [registry, client.register]
 let PROMETHEUS_REGISTRY: client.Registry | null = null
 
 export default routes((router) => {
-  if (PROMETHEUS_BEARER_TOKEN) {
-    console.log(
-      `exposing private /metrics:`,
-      yellow(`"Authentication: Bearer ${PROMETHEUS_BEARER_TOKEN}"`)
-    )
-    router.get(
-      '/metrics',
-      withBearerToken([PROMETHEUS_BEARER_TOKEN]),
-      handleRaw(getMetrics, 'text')
-    )
-  } else {
-    console.log(`exposing public /metrics`)
-    router.get('/metrics', handleRaw(getMetrics, 'text'))
-  }
+  router.get('/metrics', withPrometheusToken({ optional: true }), handleRaw(getMetrics, 'text'))
 })
 
 export function exposeRegistry(registry: client.Registry) {
