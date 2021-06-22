9b9c3e6e5089b0ed09e36e7c25971b525e07b6a1
Fede <i@2fd.me>
2021-06-22 12:41:00 -0300

    fix: useAsyncTasks

--- src/hooks/useAsyncTasks.ts
+++ src/hooks/useAsyncTasks.ts
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 
-export type AsyncTaskIdenfity = (id: string | number, ...extra: any[]) => Promise<any>
+export type AsyncTaskIdenfity = (id: string, ...extra: any[]) => Promise<any>
 
-export default function useAsyncTasks<C extends AsyncTaskIdenfity = AsyncTaskIdenfity>(callback: C): readonly [ (string | number)[], C ] {
-  const [tasks, setTasks] = useState<[string | number, Promise<any> | null][]>([])
-  function addTask(id: string | number, ...extra: any[]) {
+export default function useAsyncTasks<C extends AsyncTaskIdenfity = AsyncTaskIdenfity>(callback: C): readonly [ (string)[], C ] {
+  const [tasks, setTasks] = useState<[string, Promise<any> | null][]>([])
+  function addTask(id: string, ...extra: any[]) {
     if (tasks.find(([currentId]) => currentId === id)) {
       return
     }
