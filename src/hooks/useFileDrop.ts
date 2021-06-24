2f2ebd5b2f36947214729907aff3a1896ff7f093
Fede <i@2fd.me>
2021-06-24 12:32:03 -0300

    fix: add useFileDrop

---
+++ src/hooks/useFileDrop.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import SingletonListener from '../utils/dom/SingletonListener'
+
+export default function useFileDrop(callback: (event: DragEvent) => void) {
+  const [ dragging, setDragging ] = useState(false)
+
+  useEffect(() => {
+    const listener = SingletonListener.from(document)
+    function onDragStart(event: DragEvent) {
+      event.preventDefault()
+      setDragging(true)
+    }
+
+    function onDragEnd(event: DragEvent) {
+      event.preventDefault()
+      setDragging(false)
+    }
+
+    function onDragOver(event: DragEvent) {
+      event.preventDefault()
+    }
+
+    function onDrop(event: DragEvent) {
+      event.preventDefault()
+      callback(event)
+    }
+
+    listener.addEventListener('dragstart', onDragStart)
+    listener.addEventListener('dragend', onDragEnd)
+    listener.addEventListener('dragover', onDragOver)
+    listener.addEventListener('drop', onDrop)
+
+    return () => {
+      listener.removeEventListener('dragstart', onDragStart)
+      listener.removeEventListener('dragend', onDragEnd)
+      listener.removeEventListener('dragover', onDragOver)
+      listener.removeEventListener('drop', onDrop)
+    }
+
+  }, [])
+
+  return dragging
+}
