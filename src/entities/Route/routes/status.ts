9018e0b9cbaf443f611dba921ae2d710d417500a
Fede <i@2fd.me>
2021-06-29 15:30:45 -0300

    feat: add extra status handler

--- src/entities/Route/routes/status.ts
+++ src/entities/Route/routes/status.ts
@@ -5,15 +5,23 @@ import routes from './routes'
 const IMAGE = env('IMAGE', `events:${Date.now()}`)
 const [image, version] = IMAGE.split(':')
 
-export default function status() {
+export default function status(extraStatusHandler?: () => Promise<Record<string, any>>) {
   return routes((router) => {
     router.get(
       '/status',
-      handle(async () => ({
-        image,
-        version,
-        timestamp: new Date(),
-      }))
+      handle(async () => {
+        let extra: Record<string, any> = {}
+        if (extraStatusHandler) {
+          extra = await extraStatusHandler()
+        }
+
+        return {
+          image,
+          version,
+          timestamp: new Date(),
+          ...extra
+        }
+      })
     )
   })
 }
