ac026e344d99ecc71af89cd8d27429dbc8ebafcb
Fede <i@2fd.me>
2021-07-13 15:45:14 -0300

    fix: remove unsed code

--- src/entities/Job/utils.ts
+++ src/entities/Job/utils.ts
@@ -1,35 +1,5 @@
-import logger from '../Development/logger'
 import { ServiceStartHandler } from '../Server/types'
-import JobContext from './context'
-import JobManager from './job'
-import { NextFunction } from './types'
-
-export function reschedule(time: number) {
-  return async (ctx: JobContext<any>, next: NextFunction) => {
-    await next()
-    if (ctx.handler) {
-      await ctx.schedule(ctx.handler, new Date(Date.now() + time), ctx.payload)
-    }
-  }
-}
-
-export function log() {
-  return async (ctx: JobContext<any>, next: NextFunction) => {
-    const startAt = Date.now()
-    await next()
-    const time = Date.now() - startAt
-    const seconds = (time / 1000).toFixed(3)
-    if (ctx.handler) {
-      logger.log(`Job "${ctx.handler}" (id: "${ctx.id}") completed! time: ${seconds}s`, {
-        id: ctx.id,
-        handler: ctx.handler,
-        time
-      })
-    } else {
-      logger.log(`Cron completed! time: ${seconds}s`, { handler: 'cron', time })
-    }
-  }
-}
+import JobManager from './manager'
 
 export const jobInitializer = (manager: JobManager): ServiceStartHandler => {
   return async () => {
