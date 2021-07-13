014c09782ec75fc7461492413226cbe3e713b8be
Fede <i@2fd.me>
2021-07-13 15:57:47 -0300

    fix: exports

--- src/entities/Job/index.ts
+++ src/entities/Job/index.ts
@@ -1,6 +1,10 @@
 import JobManager from './manager'
+import { Job, createJob } from './job'
+import { jobInitializer } from './utils'
 import { JobSettings } from './types'
 
 export default function job(options: JobSettings = {}) {
   return new JobManager(options)
 }
+
+export { JobSettings, Job, createJob, jobInitializer }
