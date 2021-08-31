4305207c1158191930e65d3e1bab7e183534f95b
frami <i@2fd.me>
2021-08-31 14:04:35 +0100

    test: cover utils/iterator

--- src/utils/iterator/range.test.ts
+++ src/utils/iterator/range.test.ts
@@ -1,35 +1,45 @@
 import range from './range'
 
-test('utils/iterator/range', () => {
-  expect(Array.from(range(0, 5))).toEqual([0, 1, 2, 3, 4])
-  expect(Array.from(range(0, 5, 2))).toEqual([0, 2, 4])
-  expect(Array.from(range(0, -5))).toEqual([0, -1, -2, -3, -4])
-  expect(Array.from(range(0, -5, 2))).toEqual([0, -2, -4])
-  expect(Array.from(range(0, 12345, 500))).toEqual([
-    0,
-    500,
-    1000,
-    1500,
-    2000,
-    2500,
-    3000,
-    3500,
-    4000,
-    4500,
-    5000,
-    5500,
-    6000,
-    6500,
-    7000,
-    7500,
-    8000,
-    8500,
-    9000,
-    9500,
-    10000,
-    10500,
-    11000,
-    11500,
-    12000,
-  ])
+describe('utils/iterator/range', () => {
+  test('Should increase between numbers', () => {
+    expect(Array.from(range(0, 5))).toEqual([0, 1, 2, 3, 4])
+    expect(Array.from(range(0, -5))).toEqual([0, -1, -2, -3, -4])
+  })
+
+  test(`Should increase/decrease unsign a diff factor`, () => {
+
+    expect(Array.from(range(0, 5, 2))).toEqual([0, 2, 4])
+    expect(Array.from(range(0, -5, 2))).toEqual([0, -2, -4])
+    expect(Array.from(range(0, 12345, 500))).toEqual([
+      0,
+      500,
+      1000,
+      1500,
+      2000,
+      2500,
+      3000,
+      3500,
+      4000,
+      4500,
+      5000,
+      5500,
+      6000,
+      6500,
+      7000,
+      7500,
+      8000,
+      8500,
+      9000,
+      9500,
+      10000,
+      10500,
+      11000,
+      11500,
+      12000,
+    ])
+  })
+
+  test(`Should fail if diff is 0`, () => {
+    expect(() => Array.from(range(0, 1, 0))).toThrow(`inc param should be different than 0`)
+  })
 })
--- src/utils/iterator/roundRobin.test.ts
+++ src/utils/iterator/roundRobin.test.ts
@@ -1,19 +1,25 @@
 import roundRobin from './roundRobin'
 
-test('utils/iterator/roundRobin', () => {
-  const randomList = [Math.random(), Math.random(), Math.random()]
-  const getRandom = roundRobin(randomList)
+describe('utils/iterator/roundRobin', () => {
+  test(`should iterate an array infinitely`, () => {
+    const randomList = [Math.random(), Math.random(), Math.random()]
+    const getRandom = roundRobin(randomList)
 
-  expect(getRandom()).toEqual(randomList[0])
-  expect(getRandom()).toEqual(randomList[1])
-  expect(getRandom()).toEqual(randomList[2])
-  expect(getRandom()).toEqual(randomList[0])
-  expect(getRandom()).toEqual(randomList[1])
-  expect(getRandom()).toEqual(randomList[2])
-  expect(getRandom()).toEqual(randomList[0])
-  expect(getRandom()).toEqual(randomList[1])
-  expect(getRandom()).toEqual(randomList[2])
-  expect(getRandom()).toEqual(randomList[0])
-  expect(getRandom()).toEqual(randomList[1])
-  expect(getRandom()).toEqual(randomList[2])
+    expect(getRandom()).toEqual(randomList[0])
+    expect(getRandom()).toEqual(randomList[1])
+    expect(getRandom()).toEqual(randomList[2])
+    expect(getRandom()).toEqual(randomList[0])
+    expect(getRandom()).toEqual(randomList[1])
+    expect(getRandom()).toEqual(randomList[2])
+    expect(getRandom()).toEqual(randomList[0])
+    expect(getRandom()).toEqual(randomList[1])
+    expect(getRandom()).toEqual(randomList[2])
+    expect(getRandom()).toEqual(randomList[0])
+    expect(getRandom()).toEqual(randomList[1])
+    expect(getRandom()).toEqual(randomList[2])
+  })
+
+  test(`should fail if the array is empty`, () => {
+    expect(() => roundRobin([])).toThrow(`Round Robin required at least 1 item`)
+  })
 })
