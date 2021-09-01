1edadd139c1b35b25367ab5f1e5d035f677715f3
frami <i@2fd.me>
2021-09-01 18:33:03 +0200

    feat: add components/Layout/NotFound

--- bin/templates/src/pages/404.tsx
+++ bin/templates/src/pages/404.tsx
@@ -1,13 +1,9 @@
 import React from "react"
-import { Container } from "decentraland-ui/dist/components/Container/Container"
-import Title from "decentraland-gatsby/dist/components/Text/Title"
-import Paragraph from "decentraland-gatsby/dist/components/Text/Paragraph"
+import NotFound from "decentraland-gatsby/dist/components/Layout/NotFound"
 
 export default function NotFoundPage() {
-  return  <Container>
-    <div style={{ minHeight: '75vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      <Title>NOT FOUND</Title>
-      <Paragraph> You just hit a route that doesn&#39;t exist...</Paragraph>
-    </div>
-  </Container>
+  return <>
+    {/* <Navigation /> */}
+    <NotFound />
+  </>
 }
---
+++ src/components/Layout/NotFound.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { Container } from "decentraland-ui/dist/components/Container/Container"
+import Head from "../Head/Head"
+import Title from "../Text/Title"
+import Paragraph from "../Text/Paragraph"
+
+export type NotFoundProps = {
+  title?: string
+  description?: string
+  image?: string
+}
+
+export default React.memo(function NotFound (props: NotFoundProps) {
+  const title = props.title || 'Not found'
+  const description = props.description || 'You just hit a route that doesn\'t exist...'
+  const image = props.image || 'https://decentraland.org/images/decentraland.png'
+
+  return <Container>
+    <Head
+      title={title}
+      description={description}
+      image={image}
+    />
+    <div style={{ minHeight: '75vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Title style={{ textTransform: 'uppercase' }}>
+        {title}
+      </Title>
+      <Paragraph>{description}</Paragraph>
+    </div>
+  </Container>
+})
