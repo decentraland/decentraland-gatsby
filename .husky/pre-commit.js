c9a46a6e8db5c6ba2c77642a8411f9b7d4e77eab
frami <i@2fd.me>
2021-09-24 16:59:43 +0200

    chore: fix format

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,7 +1,7 @@
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [
+  '**/*.{js,jsx,ts,tsx}': [
     'jest --bail --watchAll=false --findRelatedTests --passWithNoTests',
     () => 'tsc-files --noEmit',
   ],
-  '*.{ts,tsx,js,jsx,json,md,mdx,css}': ['prettier --write'],
+  '**/*.{ts,tsx,js,jsx,json,md,mdx,css}': ['prettier --write'],
 }
