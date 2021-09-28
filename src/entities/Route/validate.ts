b7a4f1f0ebd8e94b09565d6833190aed1e153e5a
frami <i@2fd.me>
2021-09-29 00:19:04 +0200

    fix: improves error messages

--- src/entities/Route/validate.ts
+++ src/entities/Route/validate.ts
@@ -6,14 +6,35 @@ export default function validate<R extends {}>(
   body: Record<string, any> = {}
 ): R {
   if (!validator(body) && validator.errors && validator.errors.length > 0) {
-    const errors = validator.errors
-      .map((error) => `${error.dataPath.slice(1)} ${error.message!}`)
+    const messages = validator.errors
+      .map((error) => {
+        let message = ''
+        if (error.dataPath) {
+          message += error.dataPath.slice(1)
+        }
+
+        if (error.message) {
+          message += (message ? ' ' : '') + error.message
+        }
+
+        if (error.params) {
+          message += (message ? ' ' : '') + JSON.stringify(error.params)
+        }
+
+        return message
+      })
       .filter(Boolean)
 
-    throw new RequestError('Bad request', RequestError.BadRequest, {
-      errors,
-      body,
-    })
+    throw new RequestError(
+      'Invalid data was sent to the server',
+      RequestError.BadRequest,
+      {
+        code: 'bad_request',
+        errors: [...validator.errors],
+        messages,
+        body,
+      }
+    )
   }
 
   return body as R
