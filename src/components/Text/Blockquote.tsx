cb265ab5d175534c9b938527afa5d1256b77882a
frami <i@2fd.me>
2021-09-29 17:23:13 +0200

    feat: add primary, danger and error props to Blockquote

--- src/components/Text/Blockquote.tsx
+++ src/components/Text/Blockquote.tsx
@@ -4,17 +4,29 @@ import { StyleNamespace } from '../../variables'
 
 import './Blockquote.css'
 
-export type BlockquoteProps = React.HTMLAttributes<HTMLQuoteElement>
+export type BlockquoteProps = React.HTMLAttributes<HTMLQuoteElement> & {
+  primary?: boolean
+  danger?: boolean
+  error?: boolean
+}
 
-export default function Blockquote(props: BlockquoteProps) {
+export default React.memo(function Blockquote({
+  primary,
+  danger,
+  error,
+  ...props
+}: BlockquoteProps) {
   return (
     <blockquote
       {...props}
       className={TokenList.join([
         StyleNamespace,
         'Blockquote',
+        primary && 'Blockquote--primary',
+        danger && 'Blockquote--danger',
+        error && 'Blockquote--error',
         props.className,
       ])}
     />
   )
-}
+})
