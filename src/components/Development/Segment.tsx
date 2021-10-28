2f602b99c38f49f69c19bf13cbb1b7bf4b946d8d
frami <i@2fd.me>
2021-10-28 08:48:21 -0500

    fix: upgrade segment

--- src/components/Development/Segment.tsx
+++ src/components/Development/Segment.tsx
@@ -8,7 +8,7 @@ export type SegmentProps = React.Props<HTMLScriptElement> &
     analyticsJS?: string
     /** Segment key */
     segmentKey?: string
-    /** Alternative source */
+    /** @deprecated Alternative source */
     src?: string
     /** Alternative source */
     trackPage?: boolean
@@ -35,13 +35,10 @@ export default React.memo(function Segment({
       {...props}
       dangerouslySetInnerHTML={{
         __html: `
-(function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="${
-          src ||
-          analyticsJS ||
-          'https://segment.decentraland.org/v1/"+t+"/segment.min.js'
-        }";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-analytics.load("${segmentKey}"); ${(trackPage && 'analytics.page();') || ''}
-}})();`,
+        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="${segmentKey}";;analytics.SNIPPET_VERSION="4.15.3";
+        analytics.load("${segmentKey}");
+        ${(trackPage && 'analytics.page();') || ''}
+      `,
       }}
     />
   )
