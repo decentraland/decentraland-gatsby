e9a13950e1136d81887b8d625fa053fab2c61336
frami <i@2fd.me>
2021-11-08 09:45:12 -0600

    refactor: auth middleware
    BREAKING CHANGE: drop base64 authentication

--- src/entities/Auth/middleware.ts
+++ src/entities/Auth/middleware.ts
@@ -1,20 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 import verify from 'decentraland-crypto-middleware/lib/verify'
-import { ChainId } from '@dcl/schemas'
-import { AuthIdentity, AuthChain, AuthLinkType } from 'dcl-crypto/dist/types'
-import {
-  Authenticator,
-  parseEmphemeralPayload,
-} from 'dcl-crypto/dist/Authenticator'
-import { fromBase64 } from '../../utils/string/base64'
+import { AUTH_CHAIN_HEADER_PREFIX } from 'decentraland-crypto-middleware/lib/types'
 import RequestError from '../Route/error'
 import { middleware } from '../Route/handle'
-import logger from '../Development/logger'
-import {
-  AUTHORIZATION_HEADER,
-  AUTH_CHAIN_HEADER_PREFIX,
-} from '../../utils/api/API.types'
-import { getProvider } from '../Blockchain/provider'
 
 export type WithAuth<R extends Request = Request> = R & {
   auth: string | undefined
@@ -23,79 +11,6 @@ export type WithAuth<R extends Request = Request> = R & {
 
 export type AuthOptions = {
   optional?: boolean
-}
-
-function checkAuthorizationHeader(options: AuthOptions = {}) {
-  return async function (req: Request) {
-    const authorization = req.header('authorization')
-    if (!authorization && options.optional) {
-      return
-    } else if (!authorization) {
-      throw new RequestError(`Unauthorized`, RequestError.Unauthorized)
-    }
-
-    const [type, token] = authorization.split(' ')
-    if (type.toLowerCase() !== 'bearer') {
-      throw new RequestError(
-        `Invalid authorization type: "${type}"`,
-        RequestError.Unauthorized
-      )
-    }
-
-    // let identity: AuthIdentity
-    let ephemeralAddress: string
-    let authChain: AuthChain
-    try {
-      const data = fromBase64(token)
-      const identity = JSON.parse(data) as AuthIdentity | AuthChain
-      authChain = Array.isArray(identity) ? identity : identity.authChain
-      const ephemeralPayloadLink = authChain.find((link) =>
-        [
-          AuthLinkType.ECDSA_PERSONAL_EPHEMERAL,
-          AuthLinkType.ECDSA_EIP_1654_EPHEMERAL,
-        ].includes(link.type)
-      )
-      const ephemeralPayload = parseEmphemeralPayload(
-        (ephemeralPayloadLink && ephemeralPayloadLink.payload) || ''
-      )
-      ephemeralAddress = ephemeralPayload.ephemeralAddress
-    } catch (error) {
-      logger.error(`Error decoding authChain`, error)
-      throw new RequestError(
-        `Invalid authorization token`,
-        RequestError.Unauthorized
-      )
-    }
-
-    let result: { ok: boolean; message?: string } = { ok: false }
-    try {
-      result = await Authenticator.validateSignature(
-        ephemeralAddress,
-        authChain,
-        getProvider({ chainId: ChainId.ETHEREUM_MAINNET })
-      )
-    } catch (error) {
-      logger.error(error)
-      throw new RequestError(
-        `Invalid authorization token sign`,
-        RequestError.Unauthorized
-      )
-    }
-
-    if (!result.ok) {
-      throw new RequestError(
-        result.message || 'Invalid authorization data',
-        RequestError.Forbidden
-      )
-    }
-
-    const auth = Authenticator.ownerAddress(authChain).toLowerCase()
-    Object.assign(req, { auth, authMetadata: null })
-  }
-}
-
-export function withAuthorizationHeader(options: AuthOptions = {}) {
-  return middleware(checkAuthorizationHeader(options))
 }
 
 export function withChainHeader(options: AuthOptions = {}) {
@@ -112,7 +27,6 @@ export function withChainHeader(options: AuthOptions = {}) {
 }
 
 export function auth(options: AuthOptions = {}) {
-  const checkAuthorizationHeader = withAuthorizationHeader(options)
   const checkChainHeader = withChainHeader(options)
   const checkOptional = middleware(async () => {
     if (!options.optional) {
@@ -121,11 +35,6 @@ export function auth(options: AuthOptions = {}) {
   })
 
   return async (req: Request, res: Response, next: NextFunction) => {
-    const authorization = req.header(AUTHORIZATION_HEADER)
-    if (authorization) {
-      return checkAuthorizationHeader(req, res, next)
-    }
-
     const chain = req.header(AUTH_CHAIN_HEADER_PREFIX + '0')
     if (chain) {
       return checkChainHeader(req, res, next)
