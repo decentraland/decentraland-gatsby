f6237d5d26fe41411456f967c4cb764bf65b901e
frami <i@2fd.me>
2021-11-08 09:31:59 -0600

    fix: update dependencies

--- jest.transform.js
+++ jest.transform.js
@@ -1,14 +1,14 @@
-module.exports = require('babel-jest')({
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          esmodules: true,
-        },
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-})
+// module.exports = require('babel-jest').trasformer.({
+//   presets: [
+//     [
+//       '@babel/preset-env',
+//       {
+//         targets: {
+//           esmodules: true,
+//         },
+//       },
+//     ],
+//     '@babel/preset-react',
+//     '@babel/preset-typescript',
+//   ],
+// })
