a941703f73f9a6f70d2d1dc3c723ed1e47768f1d
frami <i@2fd.me>
2021-11-09 08:54:42 -0600

    fix: Markdown

---
+++ src/components/Text/List.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import TokenList from '../../utils/dom/TokenList'
+import { StyleNamespace } from '../../variables'
+
+import './Paragraph.css'
+import './List.css'
+
+export type ListProps = (
+  | React.HTMLAttributes<HTMLUListElement>
+  | React.HTMLAttributes<HTMLOListElement>
+) & {
+  ordered?: boolean
+  depth?: number
+}
+
+export default React.memo(function List(props: ListProps) {
+  if (props.ordered) {
+    return (
+      <ol
+        {...props}
+        className={TokenList.join([StyleNamespace, 'List', props.className])}
+      />
+    )
+  } else {
+    return (
+      <ul
+        {...props}
+        className={TokenList.join([StyleNamespace, 'List', props.className])}
+      />
+    )
+  }
+})
+
+export type ListItem = React.LiHTMLAttributes<HTMLLIElement>
+
+export const ListItem = React.memo(function (props: ListItem) {
+  return (
+    <li
+      {...props}
+      className={TokenList.join([StyleNamespace, 'ListItem', props.className])}
+    />
+  )
+})
--- src/components/Text/Markdown.tsx
+++ src/components/Text/Markdown.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactMarkdown, { Options, Components } from 'react-markdown'
+import gfm from 'remark-gfm'
+import emoji from 'remark-emoji'
 import { Table } from 'decentraland-ui/dist/components/Table/Table'
+import { Radio } from 'decentraland-ui/dist/components/Radio/Radio'
 import MainTitle from './MainTitle'
 import Title from './Title'
 import SubTitle from './SubTitle'
@@ -10,6 +13,7 @@ import Bold from './Bold'
 import Code from './Code'
 import Link from './Link'
 import Blockquote from './Blockquote'
+import List, { ListItem } from './List'
 
 export type MarkdownProps = Omit<
   Options,
@@ -17,60 +21,50 @@ export type MarkdownProps = Omit<
 >
 
 export const components: Components = {
-  h1: MainTitle,
-  h2: Title,
-  h3: SubTitle,
-  h4: SubTitle,
-  h5: SubTitle,
-  h6: SubTitle,
-  p: Paragraph as any,
-  i: Italic as any,
-  b: Bold as any,
-  a: Link,
-  code: Code as any,
-  // inlineCode: Code,
-  blockquote: Blockquote,
-  ol: 'ol',
-  ul: 'ul',
-  li: (props) => {
-    return (
-      <li {...props}>
-        <Paragraph>
-          {typeof props.checked === 'boolean' && (
-            <input
-              type="checkbox"
-              checked={props.checked}
-              readOnly
-              style={{ marginRight: '.5em' }}
-            />
-          )}
-          {props.children}
-        </Paragraph>
-      </li>
-    )
-  },
-  table: (props) => <Table basic="very">{props.children}</Table>,
-  th: Table.Header as any,
-  tbody: Table.Body as any,
-  thead: Table.Header as any,
-  tr: Table.Row as any,
-  // td: Table.Cell,
-  td: (props) =>
-    props.isHeader ? (
-      <Table.HeaderCell {...(props as any)} />
-    ) : (
-      <Table.Cell {...(props as any)} />
-    ),
+  h1: React.memo(({ node, level, ...props }) => <MainTitle {...props} />),
+  h2: React.memo(({ node, level, ...props }) => <Title {...props} />),
+  h3: React.memo(({ node, level, ...props }) => <SubTitle {...props} />),
+  h4: 'h4',
+  h5: 'h5',
+  h6: 'h6',
+  del: 'del',
+  p: React.memo(({ node, ...props }) => <Paragraph {...props} />),
+  strong: React.memo(({ node, ...props }) => <Bold {...(props as any)} />),
+  em: React.memo(({ node, ...props }) => <Italic {...(props as any)} />),
+  a: React.memo(({ node, ...props }) => <Link {...props} />),
+  code: React.memo(({ node, ...props }) => {
+    const result = (props.className || '').match(/^language\-(\w+)$/)
+    const language = result ? result[1] : undefined
+    return <Code language={language} {...(props as any)} />
+  }),
+  pre: React.memo(({ node, ...props }) => <pre {...props} />),
+  blockquote: React.memo(({ node, ...props }) => <Blockquote {...props} />),
+  ol: React.memo(({ node, ...props }) => <List {...props} />),
+  ul: React.memo(({ node, ...props }) => <List {...props} />),
+  li: React.memo(({ node, ...props }) => <ListItem {...props} />),
+  input: React.memo(({ node, disabled, ...props }) => (
+    <Radio readOnly={disabled} {...(props as any)} />
+  )),
+  table: React.memo(({ node, ...props }) => (
+    <Table basic="very">{props.children}</Table>
+  )),
+  tbody: React.memo(({ node, ...props }) => <Table.Body {...props} />),
+  thead: React.memo(({ node, ...props }) => <Table.Header {...props} />),
+  tr: React.memo(({ node, ...props }) => <Table.Row {...props} />),
+  th: React.memo(({ node, isHeader, ...props }) => (
+    <Table.HeaderCell {...(props as any)} />
+  )),
+  td: React.memo(({ node, isHeader, ...props }) => (
+    <Table.Cell {...(props as any)} />
+  )),
 }
 
 export const allowedTypes = ['root', 'text'].concat(Object.keys(components))
 
+export const plugins = [gfm, emoji] as any
+
 export default React.memo(function Markdown(props: MarkdownProps) {
   return (
-    <ReactMarkdown
-      {...props}
-      components={components}
-      allowedElements={allowedTypes}
-    />
+    <ReactMarkdown {...props} components={components} remarkPlugins={plugins} />
   )
 })
