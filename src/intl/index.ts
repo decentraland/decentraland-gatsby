d8e742c9f3c65a31f9ecff55dca2a5cbe45c683b
frami <i@2fd.me>
2021-11-10 22:11:36 -0600

    fix: add build:intl

---
+++ src/intl/index.ts
@@ -0,0 +1,5 @@
+export { default as en } from './en.json'
+export { default as es } from './es.json'
+export { default as fr } from './fr.json'
+export { default as ko } from './ko.json'
+export { default as zh } from './zh.json'
