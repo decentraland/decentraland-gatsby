4a9614cc1b6fe50de57cf5bb6e159599af74e05a
frami <i@2fd.me>
2021-11-13 23:33:35 -0600

    fix: expose IntlProvider

--- src/plugins/intl/index.ts
+++ src/plugins/intl/index.ts
@@ -1,2 +1,3 @@
 export { default as Link, LinkProps } from './Link'
+export { default as IntlProvider, IntlProviderProps } from './IntlProvider'
 export { navigate, back, changeLocale } from './utils'
