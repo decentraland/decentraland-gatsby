b2cb5e5ab2ac7bc664fc9b5e5bf9eed8257671f9
frami <i@2fd.me>
2021-11-16 20:14:35 -0600

    fix: Logger broadcast

--- src/entities/Development/logger.ts
+++ src/entities/Development/logger.ts
@@ -1,27 +1,84 @@
+export type LoggerLevel = 'info' | 'warning' | 'error'
+export type LoggerData = Record<string, any> & { level: LoggerLevel }
+export type LoggerSubscription = (message: string, data: LoggerData) => void
+
 export class Logger {
+  static readonly subscriptions: Map<LoggerLevel, LoggerSubscription[]> =
+    new Map()
+
+  static subscribe(level: LoggerLevel, callback: LoggerSubscription) {
+    const subscriptions = Logger.subscriptions.get(level) || []
+    Logger.subscriptions.set(level, [...subscriptions, callback])
+  }
+
+  static unsubscribe(level: LoggerLevel, callback: LoggerSubscription) {
+    const subscriptions = Logger.subscriptions.get(level) || []
+    Logger.subscriptions.set(
+      level,
+      subscriptions.filter((subscription) => subscription !== callback)
+    )
+  }
+
+  static broadcast(message: string, data: LoggerData) {
+    Promise.resolve().then((): any => {
+      const subscriptions = Logger.subscriptions.get(data.level)
+      if (subscriptions && subscriptions.length > 0) {
+        return Promise.all(
+          subscriptions.map(async (subscription) => {
+            try {
+              return Promise.resolve(subscription(message, data))
+            } catch (err) {
+              console.error(
+                JSON.stringify({
+                  log: 'Error broadcasting logs',
+                  level: 'error',
+                  message,
+                  data,
+                })
+              )
+            }
+          })
+        )
+      }
+    })
+  }
+
   constructor(private data: Record<string, any> = {}) {}
 
   extend(data: Record<string, any> = {}) {
     return new Logger({ ...this.data, ...data })
   }
 
-  private write(log: string, data: Record<string, any> = {}) {
+  subscribe(level: LoggerLevel, callback: LoggerSubscription) {
+    Logger.subscribe(level, callback)
+    return this
+  }
+
+  unsubscribe(level: LoggerLevel, callback: LoggerSubscription) {
+    Logger.unsubscribe(level, callback)
+    return this
+  }
+
+  private write(message: string, data: LoggerData) {
+    const extended = { ...data, ...this.data }
     if (process.env.NODE_ENV === 'production') {
-      console.log(JSON.stringify({ log, ...data, ...this.data }))
+      console.log(JSON.stringify({ log: message, ...extended }))
     } else {
-      console.log(log, JSON.stringify({ ...data, ...this.data }))
+      console.log(message, JSON.stringify(extended))
     }
+
+    Logger.broadcast(message, extended)
   }
 
-  log(log: string, data: Record<string, any> = {}): void {
-    return this.write(log, { level: 'info', data })
+  log(message: string, data: Record<string, any> = {}): void {
+    return this.write(message, { level: 'info', data })
   }
 
-  warning(log: string, data: Record<string, any> = {}): void {
-    return this.write(log, { level: 'warning', data })
+  warning(message: string, data: Record<string, any> = {}): void {
+    return this.write(message, { level: 'warning', data })
   }
 
-  error(log: string, data: Record<string, any> = {}): void {
+  error(message: string, data: Record<string, any> = {}): void {
     if (data instanceof Error) {
       data = {
         ...data,
@@ -30,7 +87,7 @@ export class Logger {
       }
     }
 
-    return this.write(log, { level: 'error', data })
+    return this.write(message, { level: 'error', data })
   }
 }
 
