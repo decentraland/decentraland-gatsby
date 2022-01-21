11fe9986c7c05dea99886fdfa6a80de1d79f0ca3
frami <i@2fd.me>
2022-01-21 00:47:25 -0300

    fix: update pbkdf2

--- bin/templates/gatsby-node.js
+++ bin/templates/gatsby-node.js
@@ -4,4 +4,24 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const sharp = require('sharp')
+sharp.cache(false)
+sharp.simd(false)
+
 // You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions, plugins }) => {
+  actions.setWebpackConfig({
+    // plugins: [plugins.provide({ Buffer: ['buffer/', 'Buffer'] })],
+    resolve: {
+      fallback: {
+        assert: false,
+        crypto: false,
+        http: false,
+        https: false,
+        os: false,
+        stream: false,
+        util: false,
+      },
+    },
+  })
+}
