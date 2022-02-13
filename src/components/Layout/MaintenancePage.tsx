e04b56b28bbd7dec96e88e976358587da9305d7d
frami <i@2fd.me>
2022-02-13 20:11:40 -0300

    feat: add MaintenancePage

---
+++ src/components/Layout/MaintenancePage.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container } from 'decentraland-ui/dist/components/Container/Container'
+import Paragraph from '../Text/Paragraph'
+import Title from '../Text/Title'
+
+export default function MaintenancePage() {
+  return (
+    <Container>
+      <div
+        style={{
+          minHeight: '75vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Title>Site under maintenance</Title>
+        <Paragraph>
+          {' '}
+          We will be back soon, sorry for the inconvenience...
+        </Paragraph>
+      </div>
+    </Container>
+  )
+}
