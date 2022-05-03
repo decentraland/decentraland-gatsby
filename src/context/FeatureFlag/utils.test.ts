dd19aa1405a43a08f97a7aee5f09ca3de8828935
frami <i@2fd.me>
2022-05-02 19:25:19 -0700

    test: listFeatureFlags

---
+++ src/context/FeatureFlag/utils.test.ts
@@ -0,0 +1,28 @@
+import { listFeatureFlags } from './utils'
+
+test(`listFeatureFlags`, () => {
+  const ff = {
+    flags: {
+      flag_switch: true,
+      flag_disabled: false,
+      flag_variant: true,
+    },
+    variants: {
+      flag_disabled: {
+        name: 'abc',
+        enabled: false,
+      },
+      flag_variant: {
+        name: 'xyz',
+        enabled: true,
+      },
+    },
+  }
+
+  expect(listFeatureFlags({ flags: {}, variants: {} })).toEqual([])
+  expect(listFeatureFlags(ff)).toEqual([
+    'flag_switch',
+    'flag_variant',
+    'flag_variant:xyz',
+  ])
+})
