347677429314a0ca5885ea829379ab8ca148dfe3
Braian Mellor <braianj@gmail.com>
2022-06-04 20:36:32 -0300

    feat: add ESLint. Config sort import. Add documentation

---
+++ .eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    // TODO: add react
+    // 'react',
+    'prettier',
+    'import',
+    'autofix',
+    'css-import-order',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    // TODO: adding react we will need to add the next line
+    // 'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:css-import-order/recommended',
+  ],
+  rules: {
+    // TODO: Review rules with off, in order to activate it, code review is going to be required
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    // end to review lines
+    'autofix/no-debugger': 'error',
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true, // don't want to sort import lines, use eslint-plugin-import instead
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['sibling', 'parent'], // <- Relative imports, the sibling and parent types they can be mingled together
+          'index',
+          'object',
+          'type',
+          'unknown',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+      node: {},
+      'babel-module': {},
+    },
+  },
+}
