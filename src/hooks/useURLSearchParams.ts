5dbe2a356cbe66b25703223e1d097c977c63e276
frami <i@2fd.me>
2022-07-02 06:25:30 +0200

    chore: deprecated useURLSearchParams

--- src/hooks/useURLSearchParams.ts
+++ src/hooks/useURLSearchParams.ts
@@ -3,6 +3,7 @@ import { useMemo } from 'react'
 
 import { useLocation } from '@gatsbyjs/reach-router'
 
+/** @deprecated use @gatsbyjs/reach-router */
 export default function useURLSearchParams() {
   const location = useLocation()
   return useMemo(() => new URLSearchParams(location.search), [location.search])
