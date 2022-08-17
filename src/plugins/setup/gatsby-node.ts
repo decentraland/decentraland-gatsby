40e8a808e173a6478ebfbfb4974b31ecf11721c2
frami <i@2fd.me>
2022-08-18 01:45:03 +0200

    feat: build plugins

--- src/plugins/setup/gatsby-node.ts
+++ src/plugins/setup/gatsby-node.ts
@@ -5,27 +5,27 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-const sharp = require('sharp')
-sharp.cache(false)
-sharp.simd(false)
+import { CreateWebpackConfigArgs } from 'gatsby'
 
 const BABEL_DEPENDENCIES_LOADER_RULE_TEST = String(/\.(js|mjs)$/)
-exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+export const onCreateWebpackConfig = ({
+  actions,
+  getConfig,
+}: CreateWebpackConfigArgs) => {
   const currentConfig = getConfig()
   const babelLoaderRule = currentConfig.module.rules.find(
-    (rule) => String(rule.test) === BABEL_DEPENDENCIES_LOADER_RULE_TEST
+    (rule: any) => String(rule.test) === BABEL_DEPENDENCIES_LOADER_RULE_TEST
   )
 
   if (!babelLoaderRule) {
     currentConfig.module.rules = [
       ...currentConfig.module.rules.filter(
-        (rule) => String(rule.test) !== BABEL_DEPENDENCIES_LOADER_RULE_TEST
+        (rule: any) => String(rule.test) !== BABEL_DEPENDENCIES_LOADER_RULE_TEST
       ),
       {
         ...babelLoaderRule,
-        exclude: (file) => {
-          if (/@noble\/secp256k1/.test(file || '')) {
+        exclude: (file: string) => {
+          if (typeof file === 'string' && /@noble\/secp256k1/.test(file)) {
             return false
           }
 
