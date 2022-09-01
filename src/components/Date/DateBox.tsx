1f841607b417b8de86c2f93fc2f18708437b53ba
frami <i@2fd.me>
2022-09-01 15:50:42 +0200

    feat: add year on DateBox

--- src/components/Date/DateBox.tsx
+++ src/components/Date/DateBox.tsx
@@ -10,6 +10,7 @@ export type DateBoxProps = {
 }
 
 export default React.memo(function DateBox(props: DateBoxProps) {
+  const now = useMemo(() => Time.from(new Date(), { utc: props.utc }), [])
   const date = useMemo(
     () => Time.from(props.date, { utc: props.utc }),
     [props.date.getTime(), props.utc]
@@ -19,6 +20,9 @@ export default React.memo(function DateBox(props: DateBoxProps) {
     <div className="DateBox">
       <div className="DateBox__Month">{date.format('MMM')}</div>
       <div className="DateBox__Day">{date.format('DD')}</div>
+      {now.year() !== date.year() && (
+        <div className="DateBox__Year">{date.format('YYYY')}</div>
+      )}
     </div>
   )
 })
