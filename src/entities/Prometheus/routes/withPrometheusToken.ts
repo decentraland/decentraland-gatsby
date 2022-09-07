9407795f75e12aa99ca1db9794d592518df85aac
frami <i@2fd.me>
2022-09-08 01:24:43 +0200

    test: withPrometheusToken

--- src/entities/Prometheus/routes/withPrometheusToken.ts
+++ src/entities/Prometheus/routes/withPrometheusToken.ts
@@ -1,5 +1,6 @@
 import env from '../../../utils/env'
 import withBearerToken from '../../Auth/routes/withBearerToken'
+import Context from '../../Route/wkc/context/Context'
 
 export function withPrometheusToken() {
   const PROMETHEUS_BEARER_TOKEN = env('PROMETHEUS_BEARER_TOKEN', '')
@@ -8,7 +9,7 @@ export function withPrometheusToken() {
     return withBearerToken({ tokens: [PROMETHEUS_BEARER_TOKEN] })
   } else {
     console.log(`Missing environment variable PROMETHEUS_BEARER_TOKEN`)
-    return async () => null
+    return async (_ctx: Pick<Context, 'request'>) => null
   }
 }
 
