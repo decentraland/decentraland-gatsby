fff7edece51317cb09c1a00cc22e1d3323164769
frami <i@2fd.me>
2022-09-12 00:18:13 +0200

    fix: FetchError

--- src/utils/errors/FetchError.ts
+++ src/utils/errors/FetchError.ts
@@ -1,10 +1,10 @@
 export default class FetchError extends Error {
   code = 'FETCH_ERROR'
-  constructor(
-    public url: string,
-    public options: RequestInit = {},
-    message: string
-  ) {
+  url: string
+  options: RequestInit = {}
+  constructor(url: string, options: RequestInit = {}, message: string) {
     super(`Fail to fetch resource "${options.method} ${url}": ${message}`)
+    this.url = url
+    this.options = options
   }
 }
