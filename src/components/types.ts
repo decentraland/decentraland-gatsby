351b12d1beb65c092abb5636f94dce0bbc3b35e1
frami <i@2fd.me>
2023-07-03 09:22:27 -0400

    refactor!: move variables to components/types

---
+++ src/components/types.ts
@@ -0,0 +1,1 @@
+export const STYLE_NAMESPACES = 'dgatsby' // Decentraland Gatsby
--- src/variables.ts
+++
@@ -1,2 +0,0 @@
-// TODO(#323): remove on v6, move to ./dom/types.ts
-export const StyleNamespace = 'dg' // Decentraland Gatsby
