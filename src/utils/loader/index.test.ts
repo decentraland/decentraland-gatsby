6e88511dffebabacb55ab0ea3117968da74b8502
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-08-03 18:26:13 -0300

    fix: Mock graph calls

--- src/utils/loader/index.test.ts
+++ src/utils/loader/index.test.ts
@@ -6,6 +6,19 @@ import { fetchLandBalance } from './landBalance'
 import manaBalance from './manaBalance'
 
 describe('manaBalance', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue({
+        data: {
+          accounts: [
+            {
+              mana: 1,
+            },
+          ],
+        },
+      }),
+    } as unknown as Response)
+  })
   test('should load mana balance', async () => {
     const balance = await manaBalance(ChainId.ETHEREUM_MAINNET).load(
       '0x9a6ebe7e2a7722f8200d0ffb63a1f6406a0d7dce'
@@ -15,6 +28,19 @@ describe('manaBalance', () => {
 })
 
 describe('fetchEnsBalance', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue({
+        data: {
+          nfts: [
+            {
+              tokenId: 'aTokenId',
+            },
+          ],
+        },
+      }),
+    } as unknown as Response)
+  })
   test('should load ens balance', async () => {
     const balance = await fetchEnsBalance(
       '0x4eac6325e1dbf1ac90434d39766e164dca71139e',
@@ -25,6 +51,20 @@ describe('fetchEnsBalance', () => {
 })
 
 describe('fetchLandBalance', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue({
+        data: {
+          nfts: [
+            {
+              tokenId: 'aTokenId',
+            },
+          ],
+        },
+      }),
+    } as unknown as Response)
+  })
+
   test('should load ens balance', async () => {
     const balance = await fetchLandBalance(
       '0x4eac6325e1dbf1ac90434d39766e164dca71139e',
@@ -35,6 +75,16 @@ describe('fetchLandBalance', () => {
 })
 
 describe('fetchEstateBalance', () => {
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue({
+        data: {
+          nfts: [{ searchEstateSize: 1 }],
+        },
+      }),
+    } as unknown as Response)
+  })
+
   test('should load ens balance', async () => {
     const [estateBalance, landBalance] = await fetchEstateBalance(
       '0x4eac6325e1dbf1ac90434d39766e164dca71139e',
