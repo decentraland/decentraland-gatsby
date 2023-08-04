1908a4ccd310c9d1811865799ad7f2c4ffd4c5c4
Gabriel Diaz <gabrieldiaz31@gmail.com>
2023-08-04 10:25:25 -0300

    fix: Update address used by tests

--- src/utils/loader/index.test.ts
+++ src/utils/loader/index.test.ts
@@ -6,19 +6,6 @@ import { fetchLandBalance } from './landBalance'
 import manaBalance from './manaBalance'
 
 describe('manaBalance', () => {
-  beforeEach(() => {
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue({
-        data: {
-          accounts: [
-            {
-              mana: 1,
-            },
-          ],
-        },
-      }),
-    } as unknown as Response)
-  })
   test('should load mana balance', async () => {
     const balance = await manaBalance(ChainId.ETHEREUM_MAINNET).load(
       '0x9a6ebe7e2a7722f8200d0ffb63a1f6406a0d7dce'
@@ -28,19 +15,6 @@ describe('manaBalance', () => {
 })
 
 describe('fetchEnsBalance', () => {
-  beforeEach(() => {
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue({
-        data: {
-          nfts: [
-            {
-              tokenId: 'aTokenId',
-            },
-          ],
-        },
-      }),
-    } as unknown as Response)
-  })
   test('should load ens balance', async () => {
     const balance = await fetchEnsBalance(
       '0x4eac6325e1dbf1ac90434d39766e164dca71139e',
@@ -51,23 +25,9 @@ describe('fetchEnsBalance', () => {
 })
 
 describe('fetchLandBalance', () => {
-  beforeEach(() => {
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue({
-        data: {
-          nfts: [
-            {
-              tokenId: 'aTokenId',
-            },
-          ],
-        },
-      }),
-    } as unknown as Response)
-  })
-
   test('should load ens balance', async () => {
     const balance = await fetchLandBalance(
-      '0x4eac6325e1dbf1ac90434d39766e164dca71139e',
+      '0x5188e308fee25ac49c10f9fd9270d953c4822ce5',
       ChainId.ETHEREUM_MAINNET
     )
     expect(balance).toBeGreaterThan(0)
@@ -75,19 +35,9 @@ describe('fetchLandBalance', () => {
 })
 
 describe('fetchEstateBalance', () => {
-  beforeEach(() => {
-    jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue({
-        data: {
-          nfts: [{ searchEstateSize: 1 }],
-        },
-      }),
-    } as unknown as Response)
-  })
-
   test('should load ens balance', async () => {
     const [estateBalance, landBalance] = await fetchEstateBalance(
-      '0x4eac6325e1dbf1ac90434d39766e164dca71139e',
+      '0x5188e308fee25ac49c10f9fd9270d953c4822ce5',
       ChainId.ETHEREUM_MAINNET
     )
     expect(estateBalance).toBeGreaterThan(0)
