dff59c740f1eb4521b3358a457c86781c8aa1511
frami <i@2fd.me>
2023-08-25 11:23:49 -0300

    docs: add npm recommendations (#679)
    * fix: autoboot Intercom
    
    * docs: add npm recommendations
    
    * chore: rename browserslistrc
    
    * refactor: replace uuid with native randomUUID
    
    * docs: move eslint to docs/guide
    
    * docs: rename docs/guide as docs/pages
    
    * docs: run markdownlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 /**
  * in case you would like to update the rules please update the documentation
- * in formatting sections in here: file://./docs/guide/typescript.md
+ * in formatting sections in here: file://./docs/pages/typescript.md
  */
 module.exports = {
   extends: ['@dcl/eslint-config/gatsby'],
