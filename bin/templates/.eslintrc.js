9f4daab8621dd614954a41ea97bacfc39161ba64
frami <i@2fd.me>
2023-09-08 11:55:26 -0300

    chore: remove project templates in favor of decentraland-gatsby-template

--- bin/templates/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-/**
- * in case you would like to update the rules please update the documentation
- * in formatting sections in here: file://./docs/pages/typescript.md
- */
-module.exports = {
-  extends: ['@dcl/eslint-config/gatsby'],
-}
--- bin/templates/src/modules/locations.ts
+++
@@ -1,8 +0,0 @@
-import API from 'decentraland-gatsby/dist/utils/api/API'
-import env from 'decentraland-gatsby/dist/utils/env'
-
-const GATSBY_BASE_URL = env('GATSBY_BASE_URL', '/')
-
-export default {
-  home: () => API.url(GATSBY_BASE_URL, '/'),
-}
