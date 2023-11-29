97664756581a82b8668616077088ba91aa54d3e2
Lautaro Petaccio <lausuper@gmail.com>
2023-11-29 12:58:00 -0300

    fix: Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,4 @@
+import React from "react"
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
 import type { StorybookConfig } from '@storybook/react-webpack5'
@@ -9,6 +10,13 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(ts|tsx|js|jsx|mdx)'],
   staticDirs: ['../static'],
   webpackFinal: async (config) => {
+    if (parseInt(React.version) <= 18) {
+      config.externals = ["react-dom/client"];
+    }
+    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
+    if (config?.module?.rules?.[2]) {
+      (config.module.rules[2] as any).exclude = [/node_modules\/(?!(gatsby|gatsby-script)\/)/]
+    }
     config.target = 'web'
     return config
   },
