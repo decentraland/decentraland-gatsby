676148f4ceedeb4e56dac2e4e8a702b7c189b1f2
Lautaro Petaccio <lausuper@gmail.com>
2023-11-29 15:07:19 -0300

    fix: Linting

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
 import type { StorybookConfig } from '@storybook/react-webpack5'
@@ -11,11 +11,13 @@ const config: StorybookConfig = {
   staticDirs: ['../static'],
   webpackFinal: async (config) => {
     if (parseInt(React.version) <= 18) {
-      config.externals = ["react-dom/client"];
+      config.externals = ['react-dom/client']
     }
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
     if (config?.module?.rules?.[2]) {
-      (config.module.rules[2] as any).exclude = [/node_modules\/(?!(gatsby|gatsby-script)\/)/]
+      ;(config.module.rules[2] as any).exclude = [
+        /node_modules\/(?!(gatsby|gatsby-script)\/)/,
+      ]
     }
     config.target = 'web'
     return config
