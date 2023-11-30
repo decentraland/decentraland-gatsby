066089ef8f49d1127c98e1153a745cdcf72ed095
Lautaro Petaccio <1120791+LautaroPetaccio@users.noreply.github.com>
2023-11-30 09:17:46 -0300

    fix: Path variables (#794)
    * fix: Path variables
    
    * fix: Linting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,3 +3,5 @@ require('balloon-css/balloon.min.css')
 require('decentraland-ui/src/themes/base-theme.css')
 require('decentraland-ui/src/themes/alternative/light-theme.css')
 require('../src/variables.css')
+global.__BASE_PATH__ = ''
+global.__PATH_PREFIX__ = ''
