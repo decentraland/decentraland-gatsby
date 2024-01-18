ef6370d60c74a1e37783b07ae1a0e4dcd39e76be
Lautaro Bustos <lautigbustos@gmail.com>
2024-01-18 07:43:01 -0300

    feat: bump dapps (#825)
    * feat: remove stuff
    
    * feat: bump update
    
    * feat: rename

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -9,32 +9,44 @@ import React, { useCallback } from 'react'
 
 import type { PageProps } from 'gatsby'
 
+import { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
+import { Network } from '@dcl/schemas/dist/dapps/network'
+import { Avatar } from '@dcl/schemas/dist/platform/profile/avatar'
+import {
+  DROPDOWN_MENU_BALANCE_CLICK_EVENT,
+  DROPDOWN_MENU_DISPLAY_EVENT,
+  DROPDOWN_MENU_ITEM_CLICK_EVENT,
+  DROPDOWN_MENU_SIGN_OUT_EVENT,
+} from 'decentraland-dapps/dist/containers/Navbar/constants'
 import {
   Footer,
   FooterProps,
 } from 'decentraland-ui/dist/components/Footer/Footer'
 import { Locale } from 'decentraland-ui/dist/components/LanguageIcon/LanguageIcon'
-import {
-  Navbar,
-  NavbarProps,
-} from 'decentraland-ui/dist/components/Navbar/Navbar'
+import { Navbar } from 'decentraland-ui/dist/components/Navbar/Navbar'
+import { NavbarProps } from 'decentraland-ui/dist/components/Navbar/Navbar.types'
+import { ManaBalancesProps } from 'decentraland-ui/dist/components/UserMenu/ManaBalances/ManaBalances.types'
+import { config } from 'decentraland-ui/dist/config'
 
 import useAuthContext from '../../context/Auth/useAuthContext'
+import useProfileInjected from '../../context/Auth/useProfileContext'
 import { getSupportedChainIds } from '../../context/Auth/utils'
 import { useFeatureFlagContext } from '../../context/FeatureFlag'
 import { DappsFeatureFlags } from '../../context/FeatureFlag/types'
 import useShareContext from '../../context/Share/useShareContext'
 import useTrackLinkContext from '../../context/Track/useTrackLinkContext'
-import useWindowScroll from '../../hooks/useWindowScroll'
+import useAsyncState from '../../hooks/useAsyncState'
+import useChainId from '../../hooks/useChainId'
 import { DecentralandIntlContext } from '../../plugins/intl/types'
 import { changeLocale } from '../../plugins/intl/utils'
+import segment from '../../utils/development/segment'
 import TokenList from '../../utils/dom/TokenList'
+import { fetchManaBalance } from '../../utils/loader/manaBalance'
 import trackEvent from '../../utils/segment/trackEvent'
 import ShareModal from '../Modal/ShareModal'
 import WalletSelectorModal from '../Modal/WalletSelectorModal'
 import WrongNetworkModal from '../Modal/WrongNetworkModal'
 
-import type { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
 import type { ProviderType } from '@dcl/schemas/dist/dapps/provider-type'
 import type { DropdownProps } from 'semantic-ui-react/dist/commonjs/modules/Dropdown'
 
@@ -63,10 +75,8 @@ export default function Layout({
   const locale = pageContext?.intl?.locale || 'en'
   const locales = pageContext?.intl?.locales || ['en']
   const [ff] = useFeatureFlagContext()
-  const [, state] = useAuthContext()
+  const [user, userState] = useAuthContext()
   const [, shareState] = useShareContext()
-  const scroll = useWindowScroll() || null
-  const isScrolled = scroll.scrollY.get() > 0
 
   const handleChangeLocal = function (
     _: React.SyntheticEvent<HTMLElement>,
@@ -75,66 +85,167 @@ export default function Layout({
     changeLocale(data.value as string)
   }
 
-  const handleClickMenuOption = useTrackLinkContext(
-    function (event: React.MouseEvent, section: string) {
-      if (props.onClickMenuOption) {
-        props.onClickMenuOption(event, section)
+  const handleClickUserMenuOption = useTrackLinkContext(
+    function (
+      event: React.MouseEvent<HTMLElement, MouseEvent>,
+      options: {
+        eventTrackingName: string
+        url?: string
+        trackingId?: string
       }
+    ) {
+      event.preventDefault()
+      segment((analytics) => {
+        analytics.track(DROPDOWN_MENU_ITEM_CLICK_EVENT, options)
+      })
 
-      if (!event.defaultPrevented) {
-        return {
-          place: 'navbar',
-          section,
-          menu: section.split('_'),
-        }
+      if (props.onClickUserMenuItem) {
+        props.onClickUserMenuItem(event, options)
       }
 
       return null
     },
-    [props.onClickMenuOption]
+    [props.onClickUserMenuItem]
+  )
+
+  const handleClickNavbarOption = useTrackLinkContext(
+    function (
+      event: React.MouseEvent<HTMLElement, MouseEvent>,
+      options: {
+        eventTrackingName: string
+        url?: string
+        isExternal?: boolean
+      }
+    ) {
+      segment((analytics) => {
+        analytics.track('Click on Navbar', options)
+      })
+      if (props.onClickNavbarItem) {
+        props.onClickNavbarItem(event, options)
+      }
+
+      return null
+    },
+    [props.onClickNavbarItem]
   )
 
   const handleSwitchNetwork = useCallback(
-    (chainId: ChainId) => state.switchTo(chainId),
-    [state]
+    (chainId: ChainId) => userState.switchTo(chainId),
+    [userState]
   )
+
   const handleConnect = useCallback(
     (providerType: ProviderType, chainId: ChainId) =>
-      state.connect(providerType, chainId),
-    [state]
+      userState.connect(providerType, chainId),
+    [userState]
   )
-  const handleCancelConnect = useCallback(() => state.select(false), [state])
+
+  const handleCancelConnect = useCallback(
+    () => userState.select(false),
+    [userState]
+  )
+
+  const [profile, profileState] = useProfileInjected()
+  const chainId = useChainId()
+  const isAuthDappEnabled = ff.enabled(DappsFeatureFlags.AuthDappEnabled)
+  const loading = userState.loading || profileState.loading
+
+  const [manaBalances] = useAsyncState<
+    ManaBalancesProps['manaBalances']
+  >(async () => {
+    if (!user) {
+      return {}
+    }
+
+    switch (chainId) {
+      case ChainId.ETHEREUM_MAINNET: {
+        const [ETHEREUM, MATIC] = await Promise.all([
+          fetchManaBalance(user, chainId),
+          fetchManaBalance(user, ChainId.MATIC_MAINNET),
+        ])
+
+        return { ETHEREUM, MATIC }
+      }
+
+      case ChainId.ETHEREUM_SEPOLIA:
+      case ChainId.ETHEREUM_GOERLI:
+      case ChainId.ETHEREUM_RINKEBY:
+      case ChainId.ETHEREUM_ROPSTEN: {
+        const [ETHEREUM, MATIC] = await Promise.all([
+          fetchManaBalance(user, chainId),
+          fetchManaBalance(user, ChainId.MATIC_MUMBAI),
+        ])
+
+        return { ETHEREUM, MATIC }
+      }
+
+      case ChainId.MATIC_MAINNET:
+      case ChainId.MATIC_MUMBAI: {
+        const MATIC = await fetchManaBalance(user, chainId)
+        return { MATIC }
+      }
+
+      default:
+        return {}
+    }
+  }, [user, chainId])
+
+  const handleClickBalance = useCallback(
+    (
+      event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+      network: Network
+    ) => {
+      event.preventDefault()
+      segment((analytics) => {
+        analytics.track(DROPDOWN_MENU_BALANCE_CLICK_EVENT, { network })
+      })
+
+      setTimeout(() => {
+        window.open(config.get('ACCOUNT_URL'), '_blank', 'noopener')
+      }, 300)
+    },
+    []
+  )
+
+  const handleOpen = useCallback(
+    (e: React.MouseEvent<HTMLElement, MouseEvent>, track_uuid: string) => {
+      segment((analytics) => {
+        analytics.track(DROPDOWN_MENU_DISPLAY_EVENT, { track_uuid })
+      })
+    },
+    []
+  )
+
+  const handleSignOut = useCallback(
+    (e: React.MouseEvent<HTMLElement, MouseEvent>, track_uuid: string) => {
+      segment((analytics) => {
+        analytics.track(DROPDOWN_MENU_SIGN_OUT_EVENT, { track_uuid })
+      })
+      setTimeout(() => {
+        userState.disconnect()
+      }, 300)
+    },
+    [userState.disconnect]
+  )
 
   return (
     <>
       {!hideNavbar && (
         <Navbar
-          mana={props.mana}
-          address={props.address}
+          manaBalances={manaBalances as ManaBalancesProps['manaBalances']}
+          address={user || undefined}
+          avatar={(profile as Avatar) || undefined}
           activePage={props.activePage}
-          leftMenu={props.leftMenu}
-          middleMenu={props.middleMenu}
-          rightMenu={props.rightMenu}
-          i18n={props.i18n}
-          isConnected={props.isConnected}
-          isConnecting={props.isConnecting}
-          isSignIn={props.isSignIn}
-          isFullscreen={props.isFullscreen}
-          isOverlay={props.isOverlay}
-          className={TokenList.join([
-            // TODO(#323): remove on v6 use bem notation
-            'LayoutNavbarContainer',
-            'layout__navbar',
-            props.className,
-            !isScrolled && 'initial',
-          ])}
-          onSignIn={
-            ff.enabled(DappsFeatureFlags.AuthDappEnabled)
-              ? state.authorize
-              : props.onSignIn
+          isSignedIn={!!profile}
+          isSigningIn={loading}
+          onClickBalance={handleClickBalance}
+          onClickNavbarItem={handleClickNavbarOption}
+          onClickUserMenuItem={handleClickUserMenuOption}
+          onClickOpen={handleOpen}
+          onClickSignIn={
+            isAuthDappEnabled ? userState.authorize : userState.select
           }
-          onClickAccount={props.onClickAccount}
-          onClickMenuOption={handleClickMenuOption}
+          onClickSignOut={handleSignOut}
         />
       )}
       <main
@@ -149,16 +260,16 @@ export default function Layout({
       </main>
       <ShareModal data={shareState.data} onClose={shareState.close} />
       <WrongNetworkModal
-        currentNetwork={state.chainId}
-        isSwitching={state.loading}
+        currentNetwork={userState.chainId}
+        isSwitching={userState.loading}
         expectedNetwork={getSupportedChainIds()}
         onSwitchNetwork={handleSwitchNetwork}
-        providerType={state.providerType}
+        providerType={userState.providerType}
       />
       <WalletSelectorModal
-        open={state.selecting}
-        loading={state.loading}
-        error={state.error}
+        open={userState.selecting}
+        loading={userState.loading}
+        error={userState.error}
         onConnect={handleConnect}
         availableProviders={availableProviders}
         disabledWalletConnect2={!ff.enabled(DappsFeatureFlags.WalletConnectV2)}
