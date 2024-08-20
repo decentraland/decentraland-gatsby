854787551b376ead479b4d3ae9d40ec248fe9d90
Kevin Szuchet <kevin.szuchet@decentraland.org>
2024-08-20 13:26:43 +0200

    chore: Use spaces instead of tabs

--- src/entities/Slack/utils.ts
+++ src/entities/Slack/utils.ts
@@ -6,32 +6,32 @@ import logger from '../Development/logger'
 const SLACK_WEBHOOK = env('SLACK_WEBHOOK', '')
 
 if (!isURL(SLACK_WEBHOOK)) {
-	logger.log(`missing config SLACK_WEBHOOK`)
+  logger.log(`missing config SLACK_WEBHOOK`)
 }
 
 // TODO: should we use @slack/web-api?
 export async function sendToSlack(body: {}) {
-	if (!isURL(SLACK_WEBHOOK)) {
-		return
-	}
-	
-	try {
-		const response = await fetch(SLACK_WEBHOOK, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(body),
-		})
-		
-		const data = await response.text()
-		
-		if (response.status >= 400) {
-			logger.error(`Slack bad request: ${data} (${response.status})`)
-		}
-	} catch (error) {
-		if (error instanceof Error) {
-			logger.error(`Slack service error: ` + error.message, error)
-		}
-	}
+  if (!isURL(SLACK_WEBHOOK)) {
+    return
+  }
+
+  try {
+    const response = await fetch(SLACK_WEBHOOK, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+
+    const data = await response.text()
+
+    if (response.status >= 400) {
+      logger.error(`Slack bad request: ${data} (${response.status})`)
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(`Slack service error: ` + error.message, error)
+    }
+  }
 }
