4de7e469f97c6eaf67b4df39bcc327cbfe21d4b4
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-12-21 22:27:30 -0300

    fix: prettier

--- src/components/Development/EnhancedIntercom.tsx
+++ src/components/Development/EnhancedIntercom.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+
 import useAuth from '../../hooks/useAuth'
 
 declare global {
@@ -9,21 +10,21 @@ declare global {
 
 /**
  * EnhancedIntercom is a client-side only component that syncs user authentication data with Intercom.
- * 
+ *
  * This component should be used in conjunction with the base Intercom component which handles the initial script setup.
  * While the base Intercom component is added via gatsby-ssr.js, this component is included in the layout component
  * to handle user data updates.
- * 
+ *
  * Features:
  * - Updates Intercom when user authentication changes
  * - Syncs wallet address and provider type
  * - Safely handles SSR environments
- * 
+ *
  * Usage:
  * ```tsx
  * // In your layout or main component
  * import EnhancedIntercom from './Development/EnhancedIntercom'
- * 
+ *
  * function Layout({ children }) {
  *   return (
  *     <>
@@ -33,7 +34,7 @@ declare global {
  *   )
  * }
  * ```
- * 
+ *
  * @returns null - This component doesn't render anything in the DOM
  */
 export default function EnhancedIntercom() {
@@ -49,7 +50,7 @@ export default function EnhancedIntercom() {
         if (account) {
           enhancedData['Wallet'] = account.toLowerCase()
         }
-        
+
         if (providerType) {
           enhancedData['Wallet type'] = providerType
         }
