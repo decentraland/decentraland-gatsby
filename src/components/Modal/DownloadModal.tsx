f52ecc30cabf9c156ff14b6ff502099f49b914d5
Kevin Szuchet <kevin.szuchet@decentraland.org>
2025-04-08 15:03:05 +0200

    refactor: Move styled components to .styled file

--- src/components/Modal/DownloadModal.tsx
+++ src/components/Modal/DownloadModal.tsx
@@ -3,12 +3,14 @@ import React, { useCallback } from 'react'
 import { DownloadButton } from 'decentraland-ui2/dist/components/DownloadButton'
 import { Modal, ModalProps } from 'decentraland-ui2/dist/components/Modal'
 
-import { emotionStyled as styled } from 'decentraland-ui2'
-
-import Paragraph from '../../components/Text/Paragraph'
 import useFormatMessage from '../../hooks/useFormatMessage'
 import ExplorerJumpinImage from '../../images/explorer-jumpin-modal.svg'
-import Title from '../Text/Title'
+import {
+  Content,
+  ImageContainer,
+  StyledDescription,
+  StyledTitle,
+} from './DownloadModal.styled'
 
 export type DownloadModalProps = Omit<ModalProps, 'children'> & {
   title?: string
@@ -16,44 +18,6 @@ export type DownloadModalProps = Omit<ModalProps, 'children'> & {
   buttonLabel?: string
   onDownloadClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
-
-const StyledTitle = styled(Title)({
-  '&.dg.Title': {
-    fontSize: '21px',
-    fontWeight: 600,
-    fontStretch: 'normal',
-    fontStyle: 'normal',
-    lineHeight: 1.43,
-    letterSpacing: '0.3px',
-    textAlign: 'center',
-  },
-})
-
-const StyledDescription = styled(Paragraph)({
-  '&.dg.Paragraph': {
-    fontSize: '17px',
-    fontWeight: 'normal',
-    fontStretch: 'normal',
-    fontStyle: 'normal',
-    lineHeight: 1.53,
-    letterSpacing: '-0.2px',
-    textAlign: 'center',
-  },
-})
-
-const Content = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  position: 'relative',
-})
-
-const ImageContainer = styled.div({
-  '& img': {
-    maxWidth: '100%',
-    height: 'auto',
-  },
-})
 
 export default function DownloadModal(props: DownloadModalProps) {
   const {
