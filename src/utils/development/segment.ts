07a550390033fbcd7b3f4144ed2b72c9c6913991
Braian Mellor <braianj@gmail.com>
2025-07-25 07:23:35 -0300

    feat: tracking with wallets (#1189)

--- src/utils/development/segment.ts
+++ src/utils/development/segment.ts
@@ -1,4 +1,7 @@
-import { isbot } from 'isbot'
+import {
+  getAllWallets,
+  getAnalytics,
+} from 'decentraland-dapps/dist/modules/analytics/utils'
 
 import { getMouseEventData, getMouseEventName, isMeta } from '../dom/events'
 import { isBlankTarget, isLocalLink } from '../dom/links'
@@ -18,38 +21,17 @@ export type Tracker = (
 
 const emptyCallback = () => {}
 const getContext = once((): TrackContext => {
-  const ethereum = window?.ethereum as any
+  const wallets = getAllWallets()
   return {
     mobile: isMobile(),
-    wallet: !ethereum
-      ? 'none'
-      : ethereum?.isMetaMask
-      ? 'metamask'
-      : ethereum?.isDapper
-      ? 'dapper'
-      : ethereum?.isCucumber
-      ? 'cucumber'
-      : ethereum?.isTrust
-      ? 'trust'
-      : ethereum?.isToshi
-      ? 'coinbase'
-      : ethereum?.isGoWallet
-      ? 'goWallet'
-      : ethereum?.isAlphaWallet
-      ? 'alphaWallet'
-      : ethereum?.isStatus
-      ? 'status'
-      : 'other',
+    wallet: wallets.length === 0 ? 'none' : wallets.join(','),
   }
 })
 
 export default function segment(tracker: Tracker, callback?: () => void) {
-  const userAgent = window.navigator.userAgent
-
-  const isBot = isbot(userAgent)
-
-  if (typeof window !== 'undefined' && window.analytics && !isBot) {
-    tracker(window.analytics, getContext(), callback ?? emptyCallback)
+  const analytics = getAnalytics()
+  if (analytics) {
+    tracker(analytics, getContext(), callback ?? emptyCallback)
   } else if (callback) {
     Promise.resolve().then(() => callback())
   }
@@ -60,12 +42,9 @@ export function track(
   data: Record<string, any> = {},
   callback?: () => void
 ) {
-  const userAgent = window.navigator.userAgent
+  const analytics = getAnalytics()
 
-  const isBot = isbot(userAgent)
-
-  if (typeof window !== 'undefined' && window.analytics && !isBot) {
-    const analytics = window.analytics
+  if (analytics) {
     analytics.track(event, { ...getContext(), ...data }, callback)
   } else if (callback) {
     Promise.resolve().then(() => callback())
